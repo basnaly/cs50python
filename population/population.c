8686daac59b153450e1b6a20c1500b7e7b49e86a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-05-29 13:33:50 +0000

    Mon, May 29, 2023, 4:33 PM +03:00

--- population/population.c
+++ population/population.c
@@ -5,12 +5,13 @@ int main(void)
 {
     // TODO: Prompt for start size
     int start = get int("Start size: ");
+    printf("start)
 
     // TODO: Prompt for end size
     int end = get int("End size: ");
 
     // TODO: Calculate number of years until we reach threshold
-    int years = 
+    int years =
 
     // TODO: Print number of years
 }
