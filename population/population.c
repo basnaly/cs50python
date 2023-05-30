849350e447df435da83eaae1a60e79062301ae3d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-05-30 06:32:39 +0000

    Tue, May 30, 2023, 9:32 AM +03:00

--- population/population.c
+++ population/population.c
@@ -6,17 +6,32 @@ int main(void)
     // TODO: Prompt for start size
     int start;
 
-    
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while start < 9
+
+    printf("Start size: %i\n", start);
 
     // TODO: Prompt for end size
     int end;
 
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start)
 
+    printf("End size: %i\n", end);
 
     // TODO: Calculate number of years until we reach threshold
     int years;
 
-
+    while()
+    {
+        
+    }
 
     // TODO: Print number of years
 
