757f7bd2d1e6603b1adeba745b9e99d1c75b32cc
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-01 12:32:03 +0000

    Thu, Jun 1, 2023, 3:32 PM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -17,7 +17,7 @@ int main(void)
         for (char j = 0; j < size; j++)
         {
             if (size - i > j + 1)
-            {
+            { 
                 printf(" ");
             }
             else
