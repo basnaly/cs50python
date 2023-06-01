929ff83f90dc4c4bbd6d297dcae8cb37d6a4757d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-01 14:37:00 +0000

    Thu, Jun 1, 2023, 5:37 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int size;
+
+    do
+    {
+        size = get_int("Height: ")
+    }
+    while(size < 1 || size > 8)
+
+    printf("Height: %i\n", size);
+
+
 }
