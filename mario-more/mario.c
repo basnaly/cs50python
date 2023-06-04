d6ff205b27984a7eec1ebdf8d0935c6aa7b9f31f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-04 14:21:16 +0000

    Sun, Jun 4, 2023, 5:21 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -30,7 +30,7 @@ int main(void)
              printf("  ");
         for (int k = 0; k < size; k++)
         {
-            if ( k + 2 > size - i)
+            if ( size - i < k + 2)
             {
                 printf("  ");
             }
