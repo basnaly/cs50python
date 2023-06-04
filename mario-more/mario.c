8790f2eca3b2f03a1e66508a4482c7c5a00735c5
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-04 13:51:09 +0000

    Sun, Jun 4, 2023, 4:51 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,6 +5,27 @@ int main(void)
 {
     int size;
 
+    size = get_int("Height: ");
 
-    
+    do
+    {
+        printf("Height, %i\n", size);
+
+    }
+    while (size < 1 || size > 8);
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (size - i > j)
+            printf(".");
+        }
+        printf("#");
+
+        for (int k = 0; k < size; k++) {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
