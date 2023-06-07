a9fc3a3f26952468a61ef04a68f961f5e5ae219e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-07 15:55:11 +0000

    Wed, Jun 7, 2023, 6:55 PM +03:00

--- cash/cash.c
+++ cash/cash.c
@@ -14,11 +14,11 @@ int main(void)
 
     do
     {
-        cents_number = get_int("Number of cents: ");
+        cents = get_int("Number of cents: ");
     }
-    while (cents < 0)
+    while (cents < 0);
 
-    printf("Number of cents: %i\n", cents_number);
+    printf("Number of cents: %i\n", cents);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
@@ -30,16 +30,22 @@ int main(void)
     // Calculate the number of dimes to give the customer
     int dimes = calculate_dimes(cents);
 
-    dimes = cents / 10; 
-    
+    dimes = cents / 10;
+
     cents = cents - dimes * 10;
 
     // Calculate the number of nickels to give the customer
     int nickels = calculate_nickels(cents);
+
+    nickels = cents / 5;
+
     cents = cents - nickels * 5;
 
     // Calculate the number of pennies to give the customer
     int pennies = calculate_pennies(cents);
+
+    pennies = cents / 1;
+
     cents = cents - pennies * 1;
 
     // Sum coins
@@ -52,29 +58,29 @@ int main(void)
 int get_cents(void)
 {
     // TODO
-    return 0;
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
     // TODO
-    return 0;
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
     // TODO
-    return 0;
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
     // TODO
-    return 0;
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
     // TODO
-    return 0;
+    return pennies;
 }
