b09983f34cadace83b4b6b01261d1fccffc7c86c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-08 12:03:53 +0000

    Thu, Jun 8, 2023, 3:03 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -29,7 +29,7 @@ long get_number(void)
 int calc_sum(long number)
 {
     int sum;
-    for (int i = 0; i < number.length; i++)
+    for (int i = 0; number>= 10; i++)
     {
         int rem = number % 10;
         number = (number - rem) / 10;
@@ -42,11 +42,12 @@ int calc_sum(long number)
             sum = sum + rem * 2;
             if (sum > 9)
             {
-                for (int j = 0; j < sum.length; i++)
+                int sum1 = sum;
+                for (int j = 0; sum1 >= 10; i++)
                 {
                     int rem1 = sum % 10;
-                    int sum1 = (sum - rem1) / 10;
-                    sum1 = sum1 + rem1;
+                    sum1 = (sum - rem1) / 10;
+                    sum = sum1 + rem1;
                 }
             }
         }
