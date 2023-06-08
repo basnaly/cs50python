090b8079956c5307e0b6192997a1f2486dac4026
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-08 12:19:21 +0000

    Thu, Jun 8, 2023, 3:19 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -40,16 +40,16 @@ int calc_sum(long number)
         else
         {
             sum = sum + rem * 2; // 14
-            if (sum > 9)
-            {
-                int sum1 = sum; //14
-                for (int j = 0; sum1 >= 10; j++)
-                {
-                    int rem1 = sum1 % 10; //4
-                    sum1 = (sum - rem1) / 10; //1
-                    sum = sum + sum1 + rem1; // 5
-                }
-            }
+            // if (sum > 9)
+            // {
+            //     int sum1 = sum; //14
+            //     for (int j = 0; sum1 >= 10; j++)
+            //     {
+            //         int rem1 = sum1 % 10; //4
+            //         sum1 = (sum - rem1) / 10; //1
+            //         sum = sum + sum1 + rem1; // 5
+            //     }
+            // }
         }
     }
     return sum;
