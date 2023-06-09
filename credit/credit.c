a201ccc47ce10d5d6b7615cb519991fc0db8867f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-09 12:45:37 +0000

    Fri, Jun 9, 2023, 3:45 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+long get_number(void);
+int calculate_sum(long number);
 
 int main(void)
 {
+    // Ask credit card number
+    long number = get_number();
+
+    //
+    int sum = calculate_sum(long number);
 
 }
