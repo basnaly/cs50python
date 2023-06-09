6401c98add36478dfc4bbbcb7db84044f9c1311c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-09 13:11:31 +0000

    Fri, Jun 9, 2023, 4:11 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -29,11 +29,21 @@ int calculate_sum(long number)
 {
     int sum_even;
     int sum_odd;
-    for (int i = 0; i <= 0; i++) {
+    for (int i = 0; i <= 0; i++)
+    {
         if (i % 2 == 0)
         {
             sum_even = sum_even + i;
         }
+        else
+        {
+            int double = i * 2;
+            if (double > 9)
+            {
+                double = 1 + (double - 10)
+            }
+            sum_odd = sum_odd + double;
+        }
     }
-    return 0;
+    return sum_even + sum_odd;
 }
