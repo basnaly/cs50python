3222214b04a0cc886134efdcb1d10eb7c9e7135e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-10 04:16:40 +0000

    Sat, Jun 10, 2023, 7:16 AM +03:00

---
+++ credit/credit.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get a credit card number
+    long number = get_number();
+
+    // Calculate sum of credit card numver digits
+    int sum = calc_sum();
+}
+
+long get_number()
+{
+    long number;
+    do
+    {
+        number = get_long("Number: ");
+    }
+    while (number < 0);
+    return number;
+}
