72c3101bcfa98503f6bdf4d047f5294e8fc198f2
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-11 09:12:12 +0000

    Sun, Jun 11, 2023, 12:12 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(void) 
+int main(void)
 {
+    // Ask for a credit card number
     
+
+    // Calc the sum of the credit card number's digits
+
+    // Check if the credit card number is valid
+
+    // Check the type of the credit card
+
 }
