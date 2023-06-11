5a75d90a646b3b54aeb0c0b3f9d8711cd94d7b2d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-11 09:15:55 +0000

    Sun, Jun 11, 2023, 12:15 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,14 +2,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+long get_number(void);
+int get_sum(long number);
+
+
 int main(void)
 {
     // Ask for a credit card number
-    
+    long number = get_number();
 
     // Calc the sum of the credit card number's digits
+    int sum = get_sum(number)
 
     // Check if the credit card number is valid
+    bool is
 
     // Check the type of the credit card
 
