cc5080eccc25eca8f99981dc2cf7fe58b4001ced
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-14 14:02:06 +0000

    Wed, Jun 14, 2023, 5:02 PM +03:00

---
+++ practice/secret.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+
+bool check_phrase(string);
+
+int main(void)
+{
+    string phrase = get string("What's the secret phrase? ");
+    if (correct == true)
+    {
+        printf("Come on in!\n");
+    }
+}
+
+bool check_phrase(string)
+{
+    
+}
