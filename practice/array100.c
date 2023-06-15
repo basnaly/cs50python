f9565473efbc4dbf45001e031bcbf76418395eab
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-15 10:25:27 +0000

    Thu, Jun 15, 2023, 1:25 PM +03:00

--- practice/array100.c
+++ practice/array100.c
@@ -3,7 +3,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void set_array(int array[4]);
+
 int main(void)
+{
+    int a = 10;
+    int b[4] = {0, 1, 2, 3};
+    set_int(a);
+    set array(b);
+    printf("%d %d\n", a, b[0]);
+}
+
+void set_array(int array[4])
 {
     
 }
