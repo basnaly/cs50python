84e33228af24643f81949caff9829db4a4376bfe
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-17 07:41:19 +0000

    Sat, Jun 17, 2023, 10:41 AM +03:00

--- hours/hours.c
+++ hours/hours.c
@@ -27,5 +27,15 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    
+    int total = 0;
+    for (int i = 0; i < weeks; i++)
+    {
+        total = total + hours[i];
+    }
+
+    if (output == 'T')
+    {
+        return total;
+    }
+    return (float) total / weeks;
 }
