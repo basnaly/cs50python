2466858b618c112d75544046c4ab734d1c0d4336
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-17 09:35:41 +0000

    Sat, Jun 17, 2023, 12:35 PM +03:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -11,34 +11,10 @@ string replace(string letter);
 
 int main(int argc, string argv[])
 {
-    
+
 }
 
 string replace(string word)
 {
-    int length = strlen(word);
-    for (int i = 0; i < length; i++)
-
-    switch(word[i])
-    {
-        case 'a':
-            word[i] = '6';
-            break;
-
-        case 'e':
-            word[i] = '3';
-            break;
-
-        case 'i':
-            word[i] = '1';
-            break;
-
-        case 'o':
-            word[i] = '0';
-            break;
-
-        default:
-            break;
-    }
-    return word;
+   
 }
