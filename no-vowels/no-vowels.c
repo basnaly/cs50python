efaeecf8abbf4144624c9183fcd04d2253d37bc3
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-17 09:25:19 +0000

    Sat, Jun 17, 2023, 12:25 PM +03:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -11,7 +11,7 @@ string replace(string letter);
 
 int main(int argc, string argv[])
 {
-    if (argc == 0 || argc > 1)
+    if (argc == 0 || argc > 2)
     {
         printf("Error \n");
         return 1;
