ca14d75c07bec0ee60b7a25d38ec1ea644117f47
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-17 13:30:13 +0000

    Sat, Jun 17, 2023, 4:30 PM +03:00

--- password/password.c
+++ password/password.c
@@ -32,19 +32,19 @@ bool valid(string password)
     bool is_punct;
     for (int i = 0;  i < length; i++)
     {
-        if (isdigit(password[i])
+        if (isdigit(password[i]))
         {
-            is_digit == true;
+            is_digit = true;
         }
-        else if (islower(password[i])
+        else if (islower(password[i]))
         {
             is_lower == true;
         }
-        else if (isupper(password[i])
+        else if (isupper(password[i]))
         {
             is_upper == true;
         }
-        else if (ispunct(password[i])
+        else if (ispunct(password[i]))
         {
             is_punct == true;
         }
