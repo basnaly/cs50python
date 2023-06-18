a36e75eeecfb80385f66a20e2058fd9e3abe2f5d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-18 15:24:32 +0000

    Sun, Jun 18, 2023, 6:24 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -42,7 +42,7 @@ int compute_score(string word)
 
     for (int i = 0; i < length; i++)
     {
-        if ((word[i] < 65 || word[i] > 91) || (word[i] < 97 || word[i] > 122))
+        if ((word[i] < 65 || word[i] > 90) || (word[i] < 97 || word[i] > 122))
         {
             sum = 0;
         }
