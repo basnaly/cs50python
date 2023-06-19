9ddabae5a988fa2f48b3e2f6ea9c30a2133a05be
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-19 13:11:26 +0000

    Mon, Jun 19, 2023, 4:11 PM +03:00

---
+++ bulbs/bulbs.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO
+    string message = get_string("What's your message? ");
+    int length = strlen(message);
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
