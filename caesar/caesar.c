de4cdc1a46c4eb9bdcfb5d03f1a9cf707631f6ee
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-20 12:49:00 +0000

    Tue, Jun 20, 2023, 3:49 PM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,12 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
-    int k;
+
     if (argc == 1 || argc > 2)
     {
-        printf("Error ");
+        printf("Usage:  ./caesar key\n");
         return 1;
     }
+
+    string argument = argv[1];
+    int length = strlen(argument);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (!isdigit(argument[i]))
+        {
+            printf("Usage:  ./caesar key\n");
+            return 1;
+        }
+    }
+
+    // int k;
+    // k = atoi(argv[1]);
+    string plain = get_string("Plaintext: ");
+
+
+    return 0;
+
+}
+
+bool only_digits(string s)
+{
+
 }
