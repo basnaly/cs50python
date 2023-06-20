491e85db246e2ebece4b530b39ddf55ea9ead1cb
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-20 14:52:44 +0000

    Tue, Jun 20, 2023, 5:52 PM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -27,14 +27,14 @@ int main(int argc, string argv[])
 
     int k = atoi(argv[1]);
 
-    string plain_text = get_string("plaintext: ");
+    string plain_text = get_string("plaintext: "); // 10milk
     int length = strlen(plain_text);
 
     for(int i = 0; i < length; i++)
     {
         if (!isalpha(plain_text[i]))
         {
-            return plain_text[i];
+             plain_text[i];
         }
         else if (isupper(plain_text[i]) || islower(plain_text[i]))
         {
