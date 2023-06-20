23860995bff54997eed46ee7ec5a2b58c96ad4f6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-20 13:07:09 +0000

    Tue, Jun 20, 2023, 4:07 PM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -35,7 +35,7 @@ int main(int argc, string argv[])
     if (s == false)
     {
         printf("Usage:  ./caesar key\n");
-            return 1;
+        return 1;
     }
 
     return 0;
@@ -43,10 +43,16 @@ int main(int argc, string argv[])
 
 bool only_digits(string s)
 {
-    if (!isdigit(s))
-    {
-        return false;
-    }
-    return true;
+    char s = [];
+    int length = strlen(s);
 
+    for (int i = 0; i < length; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            printf("Usage:  ./caesar key\n");
+            return 1;
+        }
+        return 0;
+    }
 }
