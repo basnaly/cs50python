6eee26faa141b164a9b41ac53cd5ec8dbde8113e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-21 11:35:53 +0000

    Wed, Jun 21, 2023, 2:35 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    if (argc == 1 || argc > 2)
+    {
+        printf("Usage:  ./substitution key\n");
+        return 1;
+    }
 }
