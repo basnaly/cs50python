05319dadda94f743ae064910f96b35e0df40e9d3
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-21 11:46:39 +0000

    Wed, Jun 21, 2023, 2:46 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -19,7 +19,7 @@ bool is_alphabetic(string argument)
     int length = strlen(argument);
     for (let i = 0; i < length; i++)
     {
-        if (isalpha(argument[i]) == 0 || )
+        if (isalpha(argument[i]) == 0)
         {
             printf("Key must contain 26 characters.\n");
             return 1;
