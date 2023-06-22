4d0369280ff8af9607b0e011f46b072a3a231a36
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-22 13:09:23 +0000

    Thu, Jun 22, 2023, 4:09 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -35,11 +35,7 @@ int main(int argc, string argv[])
     }
 
     string key = argv[1];
-    int key_length = strlen(key);
-    for (int i = 0; i < key_length; i++)
-    {
-        key[i] = tolower(key[i]);
-    }
+    // TODO: 
 
     string plain_text = get_string("plaintext: ");
     int text_length = strlen(plain_text);
