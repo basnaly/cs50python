4b913b26eb193e7b5a90100130f45fb000f87ea7
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-22 11:07:19 +0000

    Thu, Jun 22, 2023, 2:07 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -47,8 +47,8 @@ int main(int argc, string argv[])
         if (isalpha(plain_text[i]) > 0)
         {
             cipher_text[i] = key[change(plain_text[i])];
-            
-            if (isupper(plain_text[i]) > 0)
+
+            if (isupper(plain_text[i]) > 0 && is)
             {
                 cipher_text[i] = cipher_text[i] + (97 - 65);
             }
