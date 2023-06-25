dac0dc13271a3f6c5c3a4898247fc95680a80777
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-25 10:28:48 +0000

    Sun, Jun 25, 2023, 1:28 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -94,7 +94,9 @@ int main(int argc, string argv[])
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
 
-        for (int )
+        for (int i = 0; i < wordsize; i++) {
+            
+        }
 
         status = {0, 0, 0};
 
