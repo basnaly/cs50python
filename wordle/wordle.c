89acc273619b1f979f85adff11ccb307897adaba
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-25 12:11:47 +0000

    Sun, Jun 25, 2023, 3:11 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -198,15 +198,15 @@ void print_word(string guess, int wordsize, int status[])
     {
         if (status[i] == 2)
         {
-            printf(GREEN"%c\n", guess[i]);
+            printf(GREEN"%c"RESET"\n", guess[i]);
         }
         else if (status[i] == 1)
         {
-            printf(YELLOW"%c\n", guess[i]);
+            printf(YELLOW"%c"RESET"\n", guess[i]);
         }
         else if (status[i] == 0)
         {
-            printf(RED"%c\n", guess[i]);
+            printf(RED"%c"RESET"\n", guess[i]);
         }
     }
 
