2e1de99c87f42e89cd849ad5c6374631e0db3f0f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-25 10:05:13 +0000

    Sun, Jun 25, 2023, 1:05 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -131,11 +131,9 @@ string get_guess(int wordsize)
     }
     while (guess_length != wordsize);
 
-    int is_upper = 0;
-
     for (int i = 0 ; i < guess_length; i++)
     {
-        if (is_upper(guess[i]))
+        if (isupper(guess[i]))
         {
             guess[i] = tolower(guess[i]);
         }
