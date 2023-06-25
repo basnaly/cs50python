2caeec8254657916e5be3b0b598d8a47c76416f0
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-25 09:32:26 +0000

    Sun, Jun 25, 2023, 12:32 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -123,7 +123,13 @@ string get_guess(int wordsize)
     // ensure users actually provide a guess that is the correct length
     // TODO #3
 
-    
+    int guess_length = strlen(guess)
+
+    do
+    {
+        guess = get_string("Input a %i-letter word:", wordsize);
+    }
+    while (guess_length != wordsize)
 
     return guess;
 }
