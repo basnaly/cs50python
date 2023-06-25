8b9dea22b25d425013cc9862dcd63c0c410a7f52
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-25 10:23:54 +0000

    Sun, Jun 25, 2023, 1:23 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -93,7 +93,8 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-        
+
+        int status[0];
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
