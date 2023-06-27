2753130c9a22b2b2487efc853a932544e8675428
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-27 12:57:34 +0000

    Tue, Jun 27, 2023, 3:57 PM +03:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -6,6 +6,8 @@
 
 int convert(string input);
 
+// int convert_loop(string input);
+
 int main(void)
 {
     string input = get_string("Enter a positive integer: ");
@@ -21,21 +23,39 @@ int main(void)
 
     // Convert string to int
     printf("%i\n", convert(input));
+
+    //printf("%i\n", convert_loop(input));
 }
 
 int convert(string input)
 {
     // TODO
 
-    int length = strlen(input)
+    long number = 0;
+    int length = strlen(input);
 
     // Define last char
     char last_char = input[length - 1];
 
     // Transform char to the int
-    int last_int = last_chat - '0';
+    int last_int = last_char - '0';
 
     // Remove last char from the string
     input[length - 1] = '\0';
-    
+
+    number = convert(input[i]) + 10 * last_int;
+    return number;
 }
+
+// int convert_loop(string input)
+// {
+//     long number = 0;
+//     int i = 0;
+
+//     while(input[i] && (input[i] >= '0' && input[i] <= '9'))
+//     {
+//         number = number * 10 + input[i] - '0';
+//         i++;
+//     }
+//     return number;
+// }
