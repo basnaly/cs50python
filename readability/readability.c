0c327c4ff8d91b400b88d7b6cb0af6424b64f7d9
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-27 12:39:15 +0000

    Tue, Jun 27, 2023, 3:39 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,19 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int count_letters(string text);
 
 int count_words(string text);
 
+int count_sentences(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
 
-    printf("%i letters\n", count_letters(text));
-    printf("%i words\n", count_words(text));
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // printf("%i letters\n", letters);
+    // printf("%i words\n", words);
+    // printf("%i sentences\n", sentences);
+
+    // Calculate the average number of letters per 100 words in the text
+    float L = (float)letters / (float)words * 100;
+
+    // Calculate the average number of sentences per 100 words in the text.
+    float S = (float)sentences / (float)words * 100;
+
+    //  Calculate the index using the Coleman-Liau formula:
+    int index = round((0.0588 * L) - (0.296 * S) - 15.8);
+
+    if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
+// Calculate letters by counting alphabetic characters in the text
 int count_letters(string text)
 {
     int total_letters = 0;
@@ -31,7 +64,34 @@ int count_letters(string text)
     return total_letters;
 }
 
+// Calculate words by counting number of spaces in the text
 int count_words(string text)
 {
+    int total_words = 1;
+    int length = strlen(text);
 
+    for (int i = 0; i < length; i++)
+    {
+        if (isspace(text[i]))
+        {
+            total_words = total_words + 1;
+        }
+    }
+    return total_words;
+}
+
+// Calculate sentences by counting '.', '!' and '?' in the text
+int count_sentences(string text)
+{
+    int total_sentences = 0;
+    int length = strlen(text);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            total_sentences = total_sentences + 1;
+        }
+    }
+    return total_sentences;
 }
