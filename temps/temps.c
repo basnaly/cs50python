70eb6d655be793b59d0c92ecbba23241369c4ba6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-28 11:05:36 +0000

    Wed, Jun 28, 2023, 2:05 PM +03:00

--- temps/temps.c
+++ temps/temps.c
@@ -65,7 +65,7 @@ int main(void)
 void sort_cities(void)
 {
     // Add your code here
-    struct sort_temps == avg_temp;
+    strncpy(sort.temp, avg_temp.temp, sizeof(NUM_CITIES));
     int length = NUM_CITIES;
 
     for (int i = 0; i < length; i++)
