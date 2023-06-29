aa646cab52a12173dc63a492d74bd389810863fa
basnaly <89012819+basnaly@users.noreply.github.com>
2023-06-29 11:09:04 +0000

    Thu, Jun 29, 2023, 2:09 PM +03:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -110,9 +110,11 @@ void add_items(void)
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
+    
+
     int length = NUM_ITEMS;
     float cost = 0;
-    
+
     for (int i = 0; i < length; i++)
     {
         if (item == menu[i].item)
