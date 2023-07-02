074d485918ce950995f336ad26e66c76d30e4f67
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-02 15:03:02 +0000

    Sun, Jul 2, 2023, 6:03 PM +03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,7 @@ bool vote(string name)
     // TODO
     for (int i = 0; i < MAX; i++)
     {
-        if (candidates[i].name == mo)
+        if (candidates[i].name == Null)
         {
              return false;
         }
