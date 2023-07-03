91b411c4139ac63eb3c27166ab8166aa9a1c9a66
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-03 12:33:51 +0000

    Mon, Jul 3, 2023, 3:33 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,11 @@ bool vote(int voter, int rank, string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidate[i].name, name) == 0)
+        if (candidate[i].name == NULL)
+        {
+            return false;
+        }
+        else if (strcmp(candidate[i].name, name) == 0)
         {
             preferences[voter][i] = rank;
             return true;
