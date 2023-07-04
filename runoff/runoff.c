292bd655b365d2cf70e397d4072900129044d70f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-04 15:49:09 +0000

    Tue, Jul 4, 2023, 6:49 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -187,7 +187,7 @@ int find_min(void)
     int min_vote = voter_count;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes < min_vote)
+        if (candidates[i].eliminated != true && candidates[i].votes < min_vote)
         {
             min_vote = candidates[i].votes;
             printf("%s: %i\n", candidates[i].name, candidates[i].votes);
