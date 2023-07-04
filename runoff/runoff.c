84ba2415c18c2d146d07732b6de041b54072b0f6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-04 14:35:21 +0000

    Tue, Jul 4, 2023, 5:35 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -171,9 +171,11 @@ bool print_winner(void)
 {
     // TODO
     for (int i = 0; i < candidate_count; i++)
-    if (candidates.votes > (voter_count /2) )
     {
-        return true;
+        if (candidates[i].votes > (voter_count / 2))
+        {
+            return true;
+        }
     }
     return false;
 }
