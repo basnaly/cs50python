b55bf77e05ac08ce6da4fe7618e0b3d4b49bea92
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-11 13:23:31 +0000

    Tue, Jul 11, 2023, 4:23 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -110,8 +110,9 @@ bool vote(int rank, string name, int ranks[])
         else if (strcmp(candidates[i], name) == 0)
         {
             ranks[rank] = i;
+
         }
-        return true;
+        
     }
     return false;
 }
