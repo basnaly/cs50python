37cc2d83e7528a5001bebac002fb6e41ed7b152d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-11 12:50:59 +0000

    Tue, Jul 11, 2023, 3:50 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -122,11 +122,12 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i; j < candidate_count; j++)
         {
-            
+            preferences[i][j] += 1;
         }
     }
+    return;
 
 }
 
