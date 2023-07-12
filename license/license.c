578f7171a844f0579f451d112cd7f4ef6cd81c65
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-12 15:09:17 +0000

    Wed, Jul 12, 2023, 6:09 PM +03:00

--- license/license.c
+++ license/license.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -31,7 +32,8 @@ int main(int argc, char *argv[])
 
         // Save plate number in array
 
-        plates[idx] = buffer;
+        plates[idx] = plate_number;
+        printf("%s\n", plate_number);
         idx++;
     }
 
