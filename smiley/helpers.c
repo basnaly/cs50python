5a7024b208b7f09fcbfd4fad2a4ab1f5b4f1ee15
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-13 10:32:13 +0000

    Thu, Jul 13, 2023, 1:32 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -4,6 +4,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
     RGBTRIPLE pixel = image[0][0];
+    for(int i = 0; i < )
     if (image[height][width] == 1)
     {
         pixel.rgbtBlue = 00001100;
