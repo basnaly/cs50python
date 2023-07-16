7a4f1094c541bc1a0cb5826b3e9369b3370fa6af
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-16 11:20:02 +0000

    Sun, Jul 16, 2023, 2:20 PM +03:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,8 +41,8 @@ int main(int argc, char *argv[])
     // Create a “buffer” to store audio samples that will be read read from the WAV file
     int16_t buffer;
 
-    //
-    FILE *input = fread(&buffer, sizeof(header), 2, input)
+    // Read the header from the input file
+    FILE *input = fread(&buffer, sizeof(header), 1, input)
 
 
     // TODO: Read samples from input file and write updated data to output file
