52aa3425dc862a869111afb78375003759d48bbe
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-16 09:31:09 +0000

    Sun, Jul 16, 2023, 12:31 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -8,8 +8,14 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < height; j++)
         {
-            if (pixel.rgbtBlue == 0 && pixel.rgbtGreen == 0 && pixel.rgbtRed == 00)
+            if ((*pixel).rgbtBlue == 0 && (*pixel).rgbtGreen == 0 && (*pixel).rgbtRed == 00)
+            {
+                (*pixel).rgbtBlue = 200;
+                (*pixel).rgbtGreen = 120;
+                (*pixel).rgbtRed = 250;
+            }
 
         }
     }
+    return 0;
 }
