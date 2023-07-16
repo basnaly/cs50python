f73137ead409237c1ea7dd18a12420febc8dda0e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-16 09:46:55 +0000

    Sun, Jul 16, 2023, 12:46 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,16 +3,17 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    RGBTRIPLE pixel = image[height][width];
+    RGBTRIPLE (&pixel) = image[height][width];
+
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
-            if (((*pixel).rgbtBlue == 0) && ((*pixel).rgbtGreen == 0) && ((*pixel).rgbtRed == 00))
+            if (((*pixel[i][j]).rgbtBlue == 0) && ((*pixel[i][j]).rgbtGreen == 0) && ((*pixel[i][j]).rgbtRed == 00))
             {
-                (*pixel).rgbtBlue = 200;
-                (*pixel).rgbtGreen = 120;
-                (*pixel).rgbtRed = 250;
+                (*pixel[i][j]).rgbtBlue = 200;
+                (*pixel[i][j]).rgbtGreen = 120;
+                (*pixel[i][j]).rgbtRed = 250;
             }
 
         }
