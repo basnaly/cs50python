58d5beb7c9a04b1b0d65883a79e47d317f1fb1b3
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-20 10:49:38 +0000

    Thu, Jul 20, 2023, 1:49 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,106 +24,27 @@ int min(int i, int j)
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            RGBTRIPLE *pixel = &image[i][j];
-            int average_pixel = round((pixel->rgbtBlue + pixel->rgbtGreen + pixel->rgbtRed) / 3);
-
-            pixel->rgbtBlue = average_pixel;
-            pixel->rgbtGreen = average_pixel;
-            pixel->rgbtRed = average_pixel;
-        }
-    }
+   
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            RGBTRIPLE *pixel = &image[i][j];
 
-            int sepia_red = round(.393 * pixel->rgbtRed + .769 * pixel->rgbtGreen + .189 * pixel->rgbtBlue);
-            if (sepia_red > 255)
-            {
-                sepia_red = 255;
-            }
-
-            int sepia_green = round(.349 * pixel->rgbtRed + .686 * pixel->rgbtGreen + .168 * pixel->rgbtBlue);
-            if (sepia_green > 255)
-            {
-                sepia_green = 255;
-            }
-
-            int sepia_blue = round(.272 * pixel->rgbtRed + .534 * pixel->rgbtGreen + .131 * pixel->rgbtBlue);
-            if (sepia_blue > 255)
-            {
-                sepia_blue = 255;
-            }
-
-            pixel->rgbtBlue = sepia_blue;
-            pixel->rgbtGreen = sepia_green;
-            pixel->rgbtRed = sepia_red;
-        }
-    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width / 2; j++)
-        {
-            RGBTRIPLE right = image[i][j];
-            RGBTRIPLE left = image[i][width - 1 - j];
 
-            image[i][width - 1 - j] = right;
-            image[i][j] = left;
-        }
-    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            RGBTRIPLE *pixel = &image[i][j];
 
-            int sum_red = 0;
-            int sum_green = 0;
-            int sum_blue = 0;
-
-            int count_pixel = 0;
-
-            // int min, max;
-
-            for (int ii = max(0, i - 1); ii <= min(i + 1, height - 1); ii++)
-            {
-                for (int jj = max(0, j - 1); jj <= min(j + 1, width - 1); jj++)
-                {
-                    sum_red += image[ii][jj].rgbtRed;
-                    count_pixel += 1;
-
-                    sum_green += image[ii][jj].rgbtGreen;
-
-                    sum_blue += image[ii][jj].rgbtBlue;
-                }
-            }
-            pixel->rgbtRed = round((float)sum_red / count_pixel);
-            pixel->rgbtBlue = round((float)sum_blue / count_pixel);
-            pixel->rgbtGreen = round((float)sum_green / count_pixel);
-        }
-    }
     return;
 }
