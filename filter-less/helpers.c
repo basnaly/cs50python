fa73b9ea15f58bdc78ef089fd78970ae3463e4a7
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-20 10:56:35 +0000

    Thu, Jul 20, 2023, 1:56 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,7 +24,14 @@ int min(int i, int j)
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-   for ()
+   for (int i = 0; i < height; i++)
+   {
+    for (int j = 0; j < width; i++)
+    {
+        RGBTRIPLE *pixel = &image[i][j];
+        
+    }
+   }
     return;
 }
 
