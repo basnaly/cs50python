94cfa3d03af0090ce567d32320b83d7d633ab381
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-20 11:27:32 +0000

    Thu, Jul 20, 2023, 2:27 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -83,11 +83,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE *pixel = &image[i][j]
             int right = image[i][j];
-            int left = image[i][j]
+            int left = image[i][length - j - 1];
         }
     }
 
