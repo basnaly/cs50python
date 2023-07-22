02e1db85aa111a88ea59fd02122a9b66d8a0b9a8
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-22 10:35:02 +0000

    Sat, Jul 22, 2023, 1:35 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -117,7 +117,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         continue;
                     }
 
-                    blur_red += pixel->rgbtRed;
+                    blur_red += pixel[ii][jj]->rgbtRed;
                     count_pixels += 1;
 
                     blur_green += pixel->rgbtGreen;
