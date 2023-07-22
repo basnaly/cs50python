b3fca21d58d63cd668dbc7e9f6d083e59be5af31
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-22 10:20:26 +0000

    Sat, Jul 22, 2023, 1:20 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -88,7 +88,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            copy[i][j] = image[i][j]
+            copy[i][j] = image[i][j];
         }
     }
 
