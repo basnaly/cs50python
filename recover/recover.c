d66830c65a01c8261fbcee62214ba4a8b2ed3854
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-22 11:43:18 +0000

    Sat, Jul 22, 2023, 2:43 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,8 @@ int main(int argc, char *argv[])
     buffer[2] == 0xff
     (buffer[3] & 0xf0) == 0xe0
 
-    // Open a new jpeg file
+    // Create a new jpeg file
+    sprintf(filename, "%03i.jpg", 2)
 
     // Write 512 bytes until a new jpeg is found
 
