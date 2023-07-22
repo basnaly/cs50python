bdd069e47516a72c61b993c9ee0f3f0170927d1c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-22 11:06:16 +0000

    Sat, Jul 22, 2023, 2:06 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,5 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    
+    // Ensure proper usage
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE");
+        return 1;
+    }
+
+    // Open card 
+    FILE *file = fopen(argv[1], "r");
 }
