c5a95b88d220dfa21be4ecae536efbe407906607
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-22 10:26:15 +0000

    Sat, Jul 22, 2023, 1:26 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -125,9 +125,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
         }
-        copy->rgbtRed = round((float)blur_red / (float)count_pixels);
-        copy->rgbtGreen = round((float)blur_green / (float)count_pixels);
-        copy->rgbtBlue = round((float)blur_blue / (float)count_pixels);
+        copy->rgbtRed = round((float) blur_red / (float) count_pixels);
+        copy->rgbtGreen = round((float) blur_green / (float) count_pixels);
+        copy->rgbtBlue = round((float) blur_blue / (float)c ount_pixels);
     }
 
     return;
