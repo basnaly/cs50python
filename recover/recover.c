1316a8d70ef805e1a082a6d02b266be320db97af
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-23 10:08:38 +0000

    Sun, Jul 23, 2023, 1:08 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -38,6 +38,7 @@ int main(int argc, char *argv[])
     if ( buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
         // Make a name for jpeg file
+        char filename[4];
         int i = 4;
         sprintf(filename, "%03i.jpg", i);
         printf(filename)
