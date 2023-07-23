7b39f4beeb84129325b5604bc95ae4408b92c2bd
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-23 10:29:35 +0000

    Sun, Jul 23, 2023, 1:29 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -63,9 +63,9 @@ int main(int argc, char *argv[])
         {
              fwrite(&buffer, sizeof(BYTE), BLOCK_SIZE, img);
         }
-
-
     }
+    fclose(img);
+    
     // Stop at end of the file, fread returns number of items were read
     if (feof(file))
         {
