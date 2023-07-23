01f405e88ea0bfaec19c828e0d2ff83305e5d5b9
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-23 10:12:47 +0000

    Sun, Jul 23, 2023, 1:12 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,6 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 typedef uint8_t BYTE;
 
@@ -33,30 +33,32 @@ int main(int argc, char *argv[])
     while (fread(&buffer, sizeof(BYTE), BLOCK_SIZE, file) == BLOCK_SIZE)
     {
 
-    // If start of new jpeg
+        // If start of new jpeg
 
         // If first jpeg...
-    // Look for beginning of jpeg and read 512 bytes into a buffer
-    if ( buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-    {
-        // Make a name for jpeg file
-        char filename[4];
-        sprintf(filename, "%03i.jpg", count_files);
-        printf("filename)
+        // Look for beginning of jpeg and read 512 bytes into a buffer
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // Make a name for jpeg file
+            char filename[8];
+            sprintf(filename, "%03i.jpg", count_files);
+            printf("%s\n", filename);
 
-        // Create a new jpeg file
-        FILE *img = fopen(filename, "w");
+            // Create a new jpeg file
+            FILE *img = fopen(filename, "w");
 
-        // Write  512 bytesof a new jpeg file
-        fwrite(buffer, sizeof(BYTE),BLOCK_SIZE, img);
+            // Write  512 bytesof a new jpeg file
+            fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, img);
 
-        fclose(img);1
-    }
+            fclose(img);
+        }
 
-    // Stop at end of the file, fread returns number of items were read
-    if (feof(file))
-    {
-         fclose(filename);
+        // Stop at end of the file, fread returns number of items were read
+        if (feof(file))
+        {
+            fclose(file);
+        }
+
     }
     return 0;
 }
