7bd71c5259b359824f8f2df86419a319d02f5eff
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-24 10:20:09 +0000

    Mon, Jul 24, 2023, 1:20 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            if 
             // Make a new jpeg file
             sprintf(filename, "%03i.jpg", count_image);
 
