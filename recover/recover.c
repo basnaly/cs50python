591c2b5cc9b11b3481ec32aefeac0c9f9f28b343
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-26 09:50:54 +0000

    Wed, Jul 26, 2023, 12:50 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,7 +27,10 @@ int main(int argc, char *argv[])
     // Read the memory card in loop and save the data in buffer
     while (fread(&buffer, sizeof(BLOCK_SIZE), BYTE, *card_file) == BLOCK_SIZE)
     {
-        if (buffer[0] = )
+        if (buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) = 0xe0)
+        {
+            
+        }
     }
 
 }
