11906206688d85dfbf9eed763ba6caf50e96009e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-26 09:37:07 +0000

    Wed, Jul 26, 2023, 12:37 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Open the memory card
     FILE *card_file = fopen(argv[1], "r");
 
     if (card_file == NULL)
@@ -23,9 +24,10 @@ int main(int argc, char *argv[])
     BYTE BLOCK_SIZE = 512;
     int buffer = 0;
 
+    // Read the memory card in loop and save the data in buffer
     while (fread(&buffer, sizeof(BLOCK_SIZE), BYTE, *card_file) == BLOCK_SIZE)
     {
-        
+
     }
 
 }
