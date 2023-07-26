3708910d61e53fc4fbb491ff1aaf9d7aa16d3bf8
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-26 10:03:05 +0000

    Wed, Jul 26, 2023, 1:03 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
         if (buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) = 0xe0)
         {
             // Create the name of the new jpeg file
+            jpeg_file = 
         }
         else
     }
