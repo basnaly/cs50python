6b6dd144a0216e525f3168e0985191f46f416e3b
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-27 14:45:36 +0000

    Thu, Jul 27, 2023, 5:45 PM +03:00

--- trie/trie.c
+++ trie/trie.c
@@ -15,8 +15,7 @@ typedef struct node
 {
     bool is_word;
     struct node *children[SIZE_OF_ALPHABET];
-}
-node;
+} node;
 
 // Function prototypes
 bool check(char *word);
@@ -108,7 +107,7 @@ int main(int argc, char *argv[])
 }
 
 // TODO: Complete the check function, return true if found, false if not found
-bool check(char* word)
+bool check(char *word)
 {
     node *cursor = root;
 
@@ -119,7 +118,7 @@ bool check(char* word)
         {
             return false;
         }
-        else 
+        else
         {
             cursor = cursor->children[index];
         }
@@ -137,7 +136,7 @@ bool unload(void)
     return true;
 }
 
-void unloader(node* current)
+void unloader(node *current)
 {
 
     // Iterate over all the children to see if they point to anything and go
