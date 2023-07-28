91e609da0794307dd57a790a2b2c28fd610d2eb3
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-28 11:28:37 +0000

    Fri, Jul 28, 2023, 2:28 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -70,12 +70,12 @@ person *create_family(int generations)
         p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        person.alleles[0] = random_allele();
-        person.alleles[1] = random_allele();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
