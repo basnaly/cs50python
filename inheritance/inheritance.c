a84f646b0ceae50e42f53e3436ef1aa7888bb58c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-28 11:11:38 +0000

    Fri, Jul 28, 2023, 2:11 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,7 +58,7 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        person.alleles[0] = random_allele();
+        person.alleles[0] = parent0.;
         person.alleles[1] = random_allele();
     }
 
