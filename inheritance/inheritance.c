59c1b1de4b96cc03649fec8215a5e5d2e135d9b0
basnaly <89012819+basnaly@users.noreply.github.com>
2023-07-29 12:20:59 +0000

    Sat, Jul 29, 2023, 3:20 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,37 +40,29 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *p = malloc(sizeof(person));
-    if (p == NULL)
-    {
-        return NULL;
-    }
+   
 
     // If there are still generations left to create
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        person *parent0 = create_family(generations - 1);
-        person *parent1 = create_family(generations - 1);
+
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = parent0;
-        p->parents[1] = parent1;
+
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = parent0->alleles[rand() % 2];
-        p->alleles[1] = parent1->alleles[rand() % 2];
+
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-     
+
 
         // TODO: Randomly assign alleles
-         p->alleles[0] = random_allele();
-         p->alleles[1] = random_allele();
+
     }
 
     // TODO: Return newly created person
