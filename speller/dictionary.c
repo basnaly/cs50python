b96279bd95155a38f75a3a2d530bba1c60bd5f49
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-01 11:56:01 +0000

    Tue, Aug 1, 2023, 2:56 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -49,7 +49,10 @@ bool load(const char *dictionary)
 
     // Read strings from file one at a time
         // fscanf(file, "%s", word)
-        fscanf(*dict_file, "%s", dict_file->word)
+        while(fscanf(*dict_file, "%s", dict_file->word))
+        {
+            
+        }
 
         // fscanf will return EOF once it reaches end of the file
         if (feof(dict_file))
