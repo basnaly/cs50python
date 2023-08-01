a917081de635084f1854ee27d8e7993e2ea85cf1
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-01 11:28:22 +0000

    Tue, Aug 1, 2023, 2:28 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -59,7 +59,8 @@ bool load(const char *dictionary)
 
     // Create a new node for each word
         // use malloc
-        
+        root = malloc(sizeof(node));
+        if 
         // remember to check if return value is NULL
         // copy word into node using strcpy
 
