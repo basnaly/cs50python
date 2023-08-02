3965de5a7d8cc9cc55ba97363ee4a653ba42d347
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-02 16:24:39 +0000

    Wed, Aug 2, 2023, 7:24 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,10 +1,12 @@
 // Implements a dictionary's functionality
 
+#include <cs50.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
