e063bbbd55e08435a5903f7ef2b2b86cbd62a79c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-04 12:06:51 +0000

    Fri, Aug 4, 2023, 3:06 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,12 +1,12 @@
 // Implements a dictionary's functionality
 
+#include <cs50.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <cs50.h>
 
 #include "dictionary.h"
 
@@ -15,8 +15,7 @@ typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
-}
-node;
+} node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 'Z' - 'A' + ('Z' - 'A') * 26 + ('Z' - 'A') * 52 + 1;
@@ -33,11 +32,11 @@ bool check(const char *word)
 
     // Access linked list at that index in the hash table;
     for (node *cursor = table[index]; cursor != NULL; cursor = cursor->next)
-        {
-            // Return true if the word is in the dictionary, false otherwise
-            if (strcasecmp(cursor->word, word) == 0)
+    {
+        // Return true if the word is in the dictionary, false otherwise
+        if (strcasecmp(cursor->word, word) == 0)
             return true;
-        }
+    }
     return false;
 }
 
@@ -70,61 +69,61 @@ bool load(const char *dictionary)
 {
     // TODO
     // Open dictionary file
-        // Use fopen
-        FILE *dict_file = fopen(dictionary, "r");
+    // Use fopen
+    FILE *dict_file = fopen(dictionary, "r");
 
-        // Remember to check if return value is null
-        if (dict_file == NULL)
-        {
-            return NULL;
-        }
+    // Remember to check if return value is null
+    if (dict_file == NULL)
+    {
+        return NULL;
+    }
 
     // Read string from file one at a time
 
-        char dict_word[LENGTH + 1];
+    char dict_word[LENGTH + 1];
 
-        // fscanf(file, "%s", word)
-        while(fscanf(dict_file, "%s", dict_word) == 1)
+    // fscanf(file, "%s", word)
+    while (fscanf(dict_file, "%s", dict_word) == 1)
+    {
+
+        // Create a new node for each word. Use malloc
+
+        node *new_node = malloc(sizeof(node));
+
+        // remember to check if return value is NULL
+        if (new_node == NULL)
         {
-
-            // Create a new node for each word. Use malloc
-
-            node *new_node = malloc(sizeof(node));
-
-                // remember to check if return value is NULL
-            if (new_node == NULL)
-            {
-                return NULL;
-            }
-                // Copy word into node using strcpy
-            strcpy(new_node->word, dict_word);
-            new_node->next = NULL;
-
-            // Hash word to obtain a hash value
-                // Use the hash function
-                // function takes a string and return the index
-            unsigned int index = hash(new_node->word);
-
-            // Insert node into hash table at that location
-                // Recall that hash table is an array of linked lists
-                // Be shure to set pointers in the correct order
-                if (table[index] == NULL)
-                {
-                    table[index] = new_node;
-                }
-                else
-                {
-                    new_node->next = table[index];
-                    table[index] = new_node;
-                }
+            return NULL;
         }
+        // Copy word into node using strcpy
+        strcpy(new_node->word, dict_word);
+        new_node->next = NULL;
 
-        // fscan will return EOF once it reaches end of file
-        if (feof(dict_file))
+        // Hash word to obtain a hash value
+        // Use the hash function
+        // function takes a string and return the index
+        unsigned int index = hash(new_node->word);
+
+        // Insert node into hash table at that location
+        // Recall that hash table is an array of linked lists
+        // Be shure to set pointers in the correct order
+        if (table[index] == NULL)
         {
-            fclose(dict_file);
-            return true;
+            table[index] = new_node;
         }
+        else
+        {
+            new_node->next = table[index];
+            table[index] = new_node;
+        }
+    }
+
+    // fscan will return EOF once it reaches end of file
+    if (feof(dict_file))
+    {
+        fclose(dict_file);
+        return true;
+    }
 
     return false;
 }
@@ -150,7 +149,6 @@ unsigned int size(void)
 bool unload(void)
 {
     // TODO
-
     for (int index = 0; index < N; index++)
     {
         node *cursor = table[index];
