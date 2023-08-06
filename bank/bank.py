4d4406b3109fc3ee7908528c36a0e23a8a36d0a2
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-06 12:39:31 +0000

    Sun, Aug 6, 2023, 3:39 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,7 @@ answer = input("Greeting: ")
 uppercase = answer.upper()
 if uppercase.startswith("HELLO"):
     print("$0")
-elif uppercase.startswith("HE"):
+elif uppercase.startswith("H"):
     print("$20")
 else:
     print("$100")
