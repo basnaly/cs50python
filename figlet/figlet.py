c0245353899a7ea405dad3e5611e841ee6ebde29
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-07 10:04:27 +0000

    Mon, Aug 7, 2023, 1:04 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,7 +9,7 @@ str = input("Input: ")
 
 if len(argv) == 0:
 
-    print("Output: " figlet.renderText(s))
+    print("Output: " figlet.renderText(str))
 elif len(argv) == 2:
     figlet.setFont(font=f) || figlet.setFont(font=font)
     print("Output: " figlet.renderText(s))
