65f355ad86ade859c152927730d380b0d2e80133
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-07 10:51:41 +0000

    Mon, Aug 7, 2023, 1:51 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -16,7 +16,7 @@ def main():
             index = randint(0, len(figlet.getFonts()))
             random_font = figlet.getFonts()[index]
             figlet.setFont(font=random_font)
-            print(f"Output: ")
+            print("Output: ")
             print(figlet.renderText(str))
         elif len(argv) == 3:
             pass
