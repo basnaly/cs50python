1b88f26350384e9cb0e483e65690bf24ed7d5289
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-07 10:18:47 +0000

    Mon, Aug 7, 2023, 1:18 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,15 +2,16 @@ from sys import argv
 from pyfiglet import Figlet
 import sys
 
+
+def main():
+
 figlet = Figlet()
 
 figlet.getFonts()
 
-def main():
-    try:
-
 str = input("Input: ")
 
+try:
 if len(argv) == 0:
     random(figlet.getFonts())
     print("Output: " figlet.renderText(str))
@@ -19,3 +20,4 @@ elif len(argv) == 2:
     # print("Output: " figlet.renderText(s))
 else:
     sys.exit("Error")
+    
