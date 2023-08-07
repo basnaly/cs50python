e9aca94095ab4b9eed5a0b6ca42ac917148ffae0
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-07 10:15:17 +0000

    Mon, Aug 7, 2023, 1:15 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -14,7 +14,7 @@ if len(argv) == 0:
     random(figlet.getFonts())
     print("Output: " figlet.renderText(str))
 elif len(argv) == 2:
-    figlet.setFont(font=f) || figlet.setFont(font=font)
-    print("Output: " figlet.renderText(s))
+    # figlet.setFont(font=f) || figlet.setFont(font=font)
+    # print("Output: " figlet.renderText(s))
 else:
-    sys.exit()
+    sys.exit("Error")
