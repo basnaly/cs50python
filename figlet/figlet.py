41efce4928e84986631caae6c6cd06751d374f88
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-08 15:56:16 +0000

    Tue, Aug 8, 2023, 6:56 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,17 +1,33 @@
 from pyfiglet import Figlet
-from sys import argv
+from sys import argv, exit
+import random
 
 
 def main():
 
     figlet = Figlet()
+    fonts = figlet.getFonts()
 
-    if (argv[1] != "-f" or argv[1] !=["--font"]) and argv[2] != nameOfFont:
-        sys.exit("Invalid usage")
+    if len(argv) == 1:
+        random_index = random.randrange(0, len(fonts))
+        random_font_name = fonts[random_index]
+        figlet.setFont(font=random_font_name)
 
-    elif len(argv) != 1 or len(argv) != 3:
-        sys.exit("Error")
+    elif len(argv) == 3:
+        if argv[1] != "-f" and argv[1] != "--font":
+            exit("Invalid usage")
+
+        elif argv[2] not in fonts:
+            exit("Invalid usage")
+
+        else:
+            figlet.setFont(font=argv[2])
+
+    else:
+        exit("Error")
 
     string = input("Input: ")
+    print("Output: ")
+    print(figlet.renderText(string))
 
-    figlet.getFonts()
+main()
