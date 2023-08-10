6c93ee6c837ff9eed358f5e396831fe08b573f80
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-10 13:49:03 +0000

    Thu, Aug 10, 2023, 4:49 PM +03:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,5 +1,20 @@
 
 
+menu = dict()
+
+menu =
+{
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
 def main():
 
     try:
