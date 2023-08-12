bc015a7a0c86d1435e55fccde8c97a47ddfbc580
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-12 10:51:20 +0000

    Sat, Aug 12, 2023, 1:51 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
 
-    with open(filename) as file:
+    with open(sys.argv[1]) as file:
       reader = csv.DictReader(file)
 
     counts = {}
