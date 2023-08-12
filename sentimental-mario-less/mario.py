1f5ca5f91a25788760a6bcd48535cd7825692230
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-12 16:34:25 +0000

    Sat, Aug 12, 2023, 7:34 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,9 +2,12 @@
 
 from cs50 import get_int
 
-number = get_int("Height: ")
-if (number < 1 or number > 8):
-    break
-else:
-    for i in range(number):
-        print("#")
+while True:
+
+    number = get_int("Height: ")
+
+    if (number < 1 or number > 8):
+        break
+    else:
+        for i in range(number):
+            print("#")
