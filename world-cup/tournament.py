7ff9f940706e24423297052086d74eea82e2e86b
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-12 11:07:31 +0000

    Sat, Aug 12, 2023, 2:07 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -21,7 +21,7 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             team = row["team"]
-            rating = row["rating"]
+            rating = int(row["rating"])
             teams.append({"team": team, "rating": rating})
     print(teams)
 
