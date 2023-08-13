4e7b8392dac5b2496b9222e6242dcf82122df69f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-13 10:13:34 +0000

    Sun, Aug 13, 2023, 1:13 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -31,7 +31,13 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     # "Uruguay": 2,
 
-    for i 
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner not in counts:
+            counts[winner] = 1
+        else:
+            counts[winner] += 1
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
