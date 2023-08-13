70e1fa7ee7ed6205979a2936a263622c3b76a1e4
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-13 09:44:32 +0000

    Sun, Aug 13, 2023, 12:44 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -61,6 +61,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    
+    winner_team = simulate_round(teams)
 
 
 if __name__ == "__main__":
