2abecb05509ae72ba4227d7a92c33670dbdc21f6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-13 15:20:40 +0000

    Sun, Aug 13, 2023, 6:20 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,3 +2,19 @@
 
 from cs50 import get_int, get_string
 
+# American Express uses 15-digit numbers
+    amex_digits = 15;
+# MasterCard uses 16-digit numbers,
+    master_digits = 16;
+# Visa uses 13- and 16-digit numbers
+    visa_digits1 = 13;
+    visa_digits2 = 16;
+
+amex_start1 = 34;
+amex_start2 = 37;
+master_start1 = 51;
+master_start2 = 52;
+master_start3 = 53;
+master_start4 = 54;
+master_start5 = 55;
+visa_start = 4;
