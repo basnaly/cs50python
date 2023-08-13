43386d0965e1528ced235b6f15e469684fa41184
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-13 14:40:42 +0000

    Sun, Aug 13, 2023, 5:40 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -13,14 +13,16 @@ while True:
         break
 
 coins = {"quaters": 0, "dimes": 0, "nickels": 0, "pennies": 0}
+rem = value
 
-if value >= QUATERS:
+if rem >= QUATERS:
     coins["quaters"] = int(value / QUATERS)
-    rem = value - coins["quaters"] * QUATERS
-    #print(rem)
+    rem = rem - coins["quaters"] * QUATERS
+    rem = round(rem * 1000) / 1000
 if rem >= DIMES:
     coins["dimes"] = int(rem / DIMES)
     rem = rem - coins["dimes"] * DIMES
+    rem = round(rem * 1000) / 1000
 if rem >= NICKELS:
     coins["nickels"] = int(rem / NICKELS)
     rem = rem - coins["nickels"] * NICKELS
@@ -28,6 +30,7 @@ if rem >= NICKELS:
 if rem >= PENNIES:
     coins["pennies"] = int(rem / PENNIES)
     rem = rem - coins["pennies"] * PENNIES
+    rem = round(rem * 1000) / 1000
 
 print(coins)
 print(rem)
