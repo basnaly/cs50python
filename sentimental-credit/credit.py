f457c9852adc8497b0c7d9ec9af3d18dcb2ab7c6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-14 14:50:45 +0000

    Mon, Aug 14, 2023, 5:50 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -24,6 +24,7 @@ def check_card(card_number):
     while (int(card_number) > 0):
         rem = int(card_number) % 10
         card_number = (int(card_number) - rem) / 10
+        print(card_number)
         if card_number % 2 == 0:
             sum = sum + rem
         else:
