6c0cfa12b5ec919e891f20740b2d5408aca82e1d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-15 13:58:58 +0000

    Tue, Aug 15, 2023, 4:58 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -19,3 +19,47 @@ visa_digits = [13, 16]
 # master_start5 = 55;
 # visa_start = 4;
 
+
+def check_number(card_number):
+    sum = 0
+    reversed_number = card_number[::-1]
+    for i in range(len(card_number)):
+        if i % 2 == 0:
+            sum = sum + int(reversed_number[i])
+        else:
+            doubled = int(reversed_number[i]) * 2
+            if doubled > 9:
+                doubled = 1 + (doubled - 10)
+            sum = sum + doubled
+    return sum
+
+
+def is_valid(sum):
+    if sum % 10 == 0:
+        return True
+    return False
+
+
+def main():
+    while True:
+        card_number = get_string("Number: ")
+        if int(card_number) > 0:
+            break
+
+    sum = check_number(card_number)
+
+    pattern_amex = re.search("^34|^37", card_number)
+    pattern_master = re.search("^51|^52|^53|^54|^55", card_number)
+    pattern_visa = re.search("^4", card_number)
+
+    if pattern_amex and len(card_number) == 15 and is_valid(sum):
+        print("AMEX")
+    elif pattern_master and len(card_number) == 16 and is_valid(sum):
+        print("MASTERCARD")
+    elif pattern_visa and len(card_number) in visa_digits and is_valid(sum):
+        print("VISA")
+    else:
+        print("INVALID")
+
+
+main()
--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,32 @@
 # TODO
+
+from cs50 import get_string
+import re
+
+
+def main():
+    text = get_string("Text: ")
+
+    letters = 0
+
+    words = len(text.split(" "))
+
+    sentences = len(re.split(r"\!|\.|\?", text)) - 1
+
+    for i in range(len(text)):
+        if text[i].isalpha():
+            letters += 1
+
+    index = round(
+        0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+    )
+
+    if index < 1:
+        print("Before Grade 1 ")
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        print("Grade ", index)
+
+
+main()
