c117359aaf1137c685567538f7540e45f8caed9f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-15 17:05:55 +0000

    Tue, Aug 15, 2023, 8:05 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -47,10 +47,10 @@ def main():
         for subsequence in dna_dict:
             print("subsequence: ", subsequence)
             print("dna_dict: ", dna_dict)
-            if dna_dict[subsequence] == db[i].subsequences:
-                print(db[i].name)
-            else:
-                print("No match ")
+            if dna_dict[subsequence] != int(object[subsequence]):
+                break
+
+    print("No match ")
 
 
     return
