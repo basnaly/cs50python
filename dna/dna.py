fa7331ed8cea002a0da4f89c70d8cdb1317da4af
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-15 14:54:43 +0000

    Tue, Aug 15, 2023, 5:54 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 
+subsequences = ["AGATC", "AATG", "TATC"]
 
 def main():
 
@@ -20,7 +21,7 @@ def main():
         sequence = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    
+
 
     # TODO: Check database for matching profiles
 
