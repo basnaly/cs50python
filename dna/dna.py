c80af16dbe73e727c380717242667e7fa922f33e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-15 14:24:58 +0000

    Tue, Aug 15, 2023, 5:24 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,9 +12,12 @@ def main():
     # TODO: Read database file into a variable
 
     with open(sys.argv[1]) as file:
-        reader = csv.DictReader(file)
+        database = csv.DictReader(file)
 
     # TODO: Read DNA sequence file into a variable
+
+    with open(sys.argv[2]) as file:
+        sequence = csv.DictReader(file)
 
     # TODO: Find longest match of each STR in DNA sequence
 
