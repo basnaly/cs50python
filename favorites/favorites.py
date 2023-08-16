405b103aaa70c9c3c0f1cb1273d3b7d871f657ab
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-16 17:12:16 +0000

    Wed, Aug 16, 2023, 8:12 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -4,7 +4,8 @@ from cs50 import SQL
 
 db = SQL("sqlite:///favorites.db")
 
-rows = db.execute(SELECT * FROM shows)
+rows = db.execute("SELECT * FROM shows")
 
 for row in rows:
-    
+    title = row.title()
+    db.execute("UPDATE shows SET title = ?", title)
