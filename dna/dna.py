6adb3b72c7818e7028862ba15b445572567b84a6
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-17 14:47:35 +0000

    Thu, Aug 17, 2023, 5:47 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -19,7 +19,7 @@ def main():
 
         for row in reader_data:
             db.append(row)
-            db_compare = list(row.values())[1:]
+            db_compare = row.values()
 
     print(db)
     print(db_compare)
