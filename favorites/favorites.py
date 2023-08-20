36492793f30ffc0dfb7bd68401ea466c91ee1e7b
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 14:40:21 +0000

    Sun, Aug 20, 2023, 5:40 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -2,12 +2,9 @@ import csv
 
 from cs50 import SQL
 
-
-
 db = SQL("sqlite:///favorites.db")
 
 rows = db.execute("SELECT * FROM shows")
-
 
 for row in rows:
     updated_title = row["title"].title()
