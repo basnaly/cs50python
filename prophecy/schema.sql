c3dd4c2c97c666df64732ec8d047613e857f6736
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:13:00 +0000

    Sun, Aug 20, 2023, 6:13 PM +03:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE houses (
 CREATE TABLE assignments (
     student_id INTEGER,
     house_id INTEGER,
-    FOREING KEY (student_id) REFERENCES students(id),
-    FOREING KEY (house_id) REFERENCES houses(id),
-    PRIMARY KEY (student_id, house_id)
+    FOREING KEY(student_id) REFERENCES students(id),
+    FOREING KEY(house_id) REFERENCES houses(id),
+    PRIMARY KEY(student_id, house_id)
 );
