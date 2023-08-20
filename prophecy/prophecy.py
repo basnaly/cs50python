a39f7738c29901a38b10c2ccf90dcc33afb3d741
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:37:08 +0000

    Sun, Aug 20, 2023, 6:37 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -7,8 +7,7 @@ with open("students.csv") as file:
 
     for row in reader:
         db.append(row)
-        db.execute("UPDATE students SET student_name = ? WHERE title = ?", row["student_name"], "")
-        db.execute("UPDATE students SET id = ? WHERE id = ?", row["id"], "")
+        db.execute("INSERT INTO students (student_name, id) VALUES row["student_name"], row["id"]")
 
         db.execute("UPDATE houses SET house_name = ? WHERE house_name = ?", row["house_name"], "")
         db.execute("UPDATE houses SET head_name = ? WHERE head_name = ?", row["head_name"], "")
