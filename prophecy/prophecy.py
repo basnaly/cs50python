71a8996429b6c0c0348f5e171d8b9c987579f90f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 16:10:23 +0000

    Sun, Aug 20, 2023, 7:10 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -10,10 +10,12 @@ with open("students.csv") as file:
     house_names = []
     for row in reader:
         db.execute("INSERT INTO students (student_name, id) VALUES (?, ?)", row["student_name"], row["id"])
-        if 
+        if row["house"] in house_names:
+            continue
+        else:
+            house_names.append(row["house"])
+            result = db.execute("INSERT INTO houses (house_name, head_name) VALUES (?, ?)", row["house"], row["head"])
 
-        result = db.execute("INSERT INTO houses (house_name, head_name) VALUES (?, ?)", row["house"], row["head"])
-            if
         print(result)
 
 
