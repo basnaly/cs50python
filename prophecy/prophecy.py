10977d71346cec768a2c76b60e5714002c7e1c36
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:25:51 +0000

    Sun, Aug 20, 2023, 6:25 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,11 +1,15 @@
 import csv
 
+from cs50 import SQL
+
 db = []
 
 with open("students.csv") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        db.append(reader)
+        db.append(row)
 
     print(db)
+
+
