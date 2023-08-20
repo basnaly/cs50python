147bb431f415a12da45f75b6de811688d14914ab
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:41:45 +0000

    Sun, Aug 20, 2023, 6:41 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -7,10 +7,10 @@ with open("students.csv") as file:
 
     for row in reader:
         db.append(row)
-        db.execute("INSERT INTO students (student_name, id) VALUES (row["student_name"], row["id"]")
+        db.execute("INSERT INTO students (student_name, id) VALUES (?, ?)", row["student_name"], row["id"])
 
-        db.execute("INSERT INTO houses (house_name, head_name) VALUES (row["house_name"], row["head_name"])
-        
+        db.execute("INSERT INTO houses (house_name, head_name) VALUES (?, ?)", row["house_name"], row["head_name"])
+
 
     print(db)
 
