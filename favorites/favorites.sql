78a8d186c07a3bb71ed55e01b8d408f2a3faff63
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 13:56:31 +0000

    Sun, Aug 20, 2023, 4:56 PM +03:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -6,4 +6,4 @@ SELECT * FROM shows;
 
 SELECT * FROM shows WHERE title = "how i met your mother";
 
-UPDATE shows SET title = "How I Met Your Mother" WHERE title = "How I Met Your Mother";
+UPDATE shows SET title = "How I Met Your Mother" WHERE title LIKE "How I Met Your Mother";
