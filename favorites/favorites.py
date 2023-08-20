4cdaf9996d382cc8deace0bbcae75cc07efdeb9a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 14:28:58 +0000

    Sun, Aug 20, 2023, 5:28 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -6,8 +6,10 @@ from cs50 import SQL
 
 db = SQL("sqlite:///favorites.db")
 
+rows = db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
+
 for row in rows:
     updated_title = row["title"].title()
-    rows = db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
+
 
 
