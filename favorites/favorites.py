ec95f64650e1317eef4c6f2d99f30c9229d4db1f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 14:37:30 +0000

    Sun, Aug 20, 2023, 5:37 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -6,10 +6,11 @@ from cs50 import SQL
 
 db = SQL("sqlite:///favorites.db")
 
-rows = db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
+rows = db.execute("SELECT * FROM shows")
+
 
 for row in rows:
     updated_title = row["title"].title()
-
+    db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
 
 
