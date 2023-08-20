24c752374eab726311cc40279014caeae40162b3
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 14:26:59 +0000

    Sun, Aug 20, 2023, 5:26 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -2,11 +2,11 @@ import csv
 
 from cs50 import SQL
 
-updated_title = row.title()
+updated_title = row["title"].title()
 
 db = SQL("sqlite:///favorites.db")
 
-rows = db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row)
-
 for row in rows:
-    
+    rows = db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
+
+
