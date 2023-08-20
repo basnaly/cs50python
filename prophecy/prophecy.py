3ce2e48ada1903f583f8254b34d62c4c0e26d090
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:46:08 +0000

    Sun, Aug 20, 2023, 6:46 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,12 +1,14 @@
 import csv
 
-db = []
+from cs50 import SQL
+
+db = SQL("sqlite:///database.db")
 
 with open("students.csv") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        db.append(row)
+
         db.execute("INSERT INTO students (student_name, id) VALUES (?, ?)", row["student_name"], row["id"])
 
         result = db.execute("INSERT INTO houses (house_name, head_name) VALUES (?, ?)", row["house_name"], row["head_name"])
