444591266d2e518b2cb9332b32c10a293288c31d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 14:38:31 +0000

    Sun, Aug 20, 2023, 5:38 PM +03:00

--- favorites/favorites.py
+++ favorites/favorites.py
@@ -11,6 +11,6 @@ rows = db.execute("SELECT * FROM shows")
 
 for row in rows:
     updated_title = row["title"].title()
-    db.execute("UPDATE shows SET title = ? WHERE title = ?", updated_title, row["title"])
+    db.execute("UPDATE shows SET title = ? WHERE title LIKE ?", updated_title, row["title"])
 
 
