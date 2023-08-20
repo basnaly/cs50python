6ac44ece68b8f1fb7cb0993d7f029229fd2107b2
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-20 15:07:58 +0000

    Sun, Aug 20, 2023, 6:07 PM +03:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -5,3 +5,22 @@ sqlite3 roster.db
 SELECT * FROM students;
 
 sqlite3 database.db
+
+CREATE TABLE students (
+    id INTEGER,
+    student_name TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE houses (
+    id INTEGER,
+    house_name TEXT,
+    head_name TEXT,
+    PRIMARY KEY(id)
+),
+
+CREATE TABLE assignments (
+    student_id INTEGER,
+    house_id INTEGER,
+    
+)
