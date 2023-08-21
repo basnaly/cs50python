17ce213fb58225d10b7bf5b54559d2aa88733235
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 11:23:05 +0000

    Mon, Aug 21, 2023, 2:23 PM +03:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,3 +1,21 @@
 sqlite3 favorites.db
 
 .schema
+
+SELECT * FROM shows;
+
++-----+------------------------------------+
+| id  |               title                |
++-----+------------------------------------+
+| 1   | How i met your mother              |
+| 2   | The Sopranos                       |
+ ...
+
+UPDATE shows SET title = "How I Met Your Mother" WHERE title LIKE "How I Met Your Mother";
+
++-----+------------------------------------+
+| id  |               title                |
++-----+------------------------------------+
+| 1   | How I Met Your Mother              |
+| 2   | The Sopranos                       |
+ ...
