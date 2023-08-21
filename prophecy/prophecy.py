10b8ec10fcf1fd6be95b992f7e3bdb207588656c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 12:32:54 +0000

    Mon, Aug 21, 2023, 3:32 PM +03:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -11,12 +11,12 @@ with open("students.csv") as file:
     for row in reader:
         print(row)
         # Add data to the student table
-        db.execute("INSERT INTO students VALUES(?, ?)", row["student_name"], row["id"])
+        db.execute("INSERT INTO students VALUES(?, ?)", row["student_name"], int(row["id"]))
 
         # Add data to the houses table
         if row["houses"] not in list_houses:
-            list_houses.append(row["houses"])
+            list_houses.append(row["house"])
             db.execute("INSERT INTO houses VALUES(?, ?)", row["house"], row["head"])
 
         # Add data to the assignments table
-        db.execute("INSERT INTO assignments VALUES(?, ?)", row["id"], list_houses.index(row["house"]) + 1)
+        db.execute("INSERT INTO assignments VALUES(?, ?)", int(row["id"]), list_houses.index(row["house"]) + 1)
