2f32199125e2ad54a426c4eb86595f26f8b3161e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 15:42:39 +0000

    Mon, Aug 21, 2023, 6:42 PM +03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,4 +1,4 @@
-SELECT songs.name FROM songs JOIN artists.name ON 
+SELECT songs.name FROM songs JOIN artists.name ON songs.artist_id = artists.id;
 
 
 SELECT table1.column_name1, table1.column_name2, table2.column_name3
