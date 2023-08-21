dd2e70f758d49844eb973990505f078226d6fd09
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 15:44:54 +0000

    Mon, Aug 21, 2023, 6:44 PM +03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,4 +1,4 @@
-SELECT songs.name, artists.name FROM songs JOIN artists ON songs.artist_id = artists.id;
+SELECT songs.name, artists.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Post Malone";
 
 
 SELECT table1.column_name1, table1.column_name2, table2.column_name3
