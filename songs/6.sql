20c18ce3530d0383bf28f991b40bf6e9acec64ce
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 15:45:46 +0000

    Mon, Aug 21, 2023, 6:45 PM +03:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,6 +1,16 @@
 SELECT songs.name, artists.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Post Malone";
 
++------------------------------+-------------+
+|             name             |    name     |
++------------------------------+-------------+
+| Better Now                   | Post Malone |
+| Candy Paint                  | Post Malone |
+| Congratulations              | Post Malone |
+| I Fall Apart                 | Post Malone |
+| Psycho (feat. Ty Dolla $ign) | Post Malone |
+| rockstar (feat. 21 Savage)   | Post Malone |
++------------------------------+-------------+
 
-SELECT table1.column_name1, table1.column_name2, table2.column_name3
-FROM table1
-INNER JOIN table2 ON table1.common_column = table2.common_column;
+-- SELECT table1.column_name1, table1.column_name2, table2.column_name3
+-- FROM table1
+-- INNER JOIN table2 ON table1.common_column = table2.common_column;
