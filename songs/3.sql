74ae2a58bb5851ce0e35549fcde1ab2cc25646e7
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 14:41:24 +0000

    Mon, Aug 21, 2023, 5:41 PM +03:00

---
+++ songs/3.sql
@@ -0,0 +1,11 @@
+SELECT name, duration_ms FROM songs ORDER BY duration_ms DESC LIMIT 5
+
++-----------------------+-------------+
+|         name          | duration_ms |
++-----------------------+-------------+
+| Te Bote - Remix       | 417920      |
+| SICKO MODE            | 312820      |
+| Walk It Talk It       | 276147      |
+| Him & I (with Halsey) | 268867      |
+| Perfect               | 263400      |
++-----------------------+-------------+
