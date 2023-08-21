f0a7fe5005a0a503dc0dbc385f50b89d126d5627
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 15:54:38 +0000

    Mon, Aug 21, 2023, 6:54 PM +03:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,16 @@
 SELECT AVG(energy) FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Drake";
+
++-------------+
+| AVG(energy) |
++-------------+
+| 0.599       |
++-------------+
+
+SELECT AVG(energy), songs.name, artists.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = "Drake";
+
++-------------+------------+-------+
+| AVG(energy) |    name    | name  |
++-------------+------------+-------+
+| 0.599       | God's Plan | Drake |
++-------------+------------+-------+
+
