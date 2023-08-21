0cbdd81bfc0217c5b54fbaf105086e34db9b1003
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 14:36:24 +0000

    Mon, Aug 21, 2023, 5:36 PM +03:00

---
+++ songs/1.sql
@@ -0,0 +1,10 @@
+SELECT name FROM songs;
+
++---------------------------------------------------------+
+|                          name                           |
++---------------------------------------------------------+
+| God's Plan                                              |
+| SAD!                                                    |
+| rockstar (feat. 21 Savage)                              |
+| Psycho (feat. Ty Dolla $ign)                            |
+ ...
