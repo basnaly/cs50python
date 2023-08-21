e713a5a5eba47d75c14192a9df5a4fd1fcd7c117
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 10:57:23 +0000

    Mon, Aug 21, 2023, 1:57 PM +03:00

--- favorites/favorites.sql
+++
@@ -1,9 +0,0 @@
-sqlite3 favorites.db
-
-.schema
-
-SELECT * FROM shows;
-
-SELECT * FROM shows WHERE title = "how i met your mother";
-
-UPDATE shows SET title = "How I Met Your Mother" WHERE title LIKE "How I Met Your Mother";
