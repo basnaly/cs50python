4fbb3bdc9c7c787ae77d4cf251a6e5227b16a204
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 14:26:24 +0000

    Mon, Aug 21, 2023, 5:26 PM +03:00

---
+++ songs/data.sql
@@ -0,0 +1,21 @@
+sqlite3 songs.db
+
+.schema
+
+CREATE TABLE songs (
+    id INTEGER,
+    name TEXT,
+    artist_id INTEGER,
+    danceability REAL,
+    energy REAL,
+    key INTEGER,
+    loudness REAL,
+    speechiness REAL,
+    valence REAL,
+    tempo REAL,
+    duration_ms INTEGER
+);
+CREATE TABLE artists (
+    id INTEGER,
+    name TEXT
+);
