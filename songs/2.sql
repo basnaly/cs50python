dc060c60994396a5a6c0a34e4ba59faf332ac2c4
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 14:36:55 +0000

    Mon, Aug 21, 2023, 5:36 PM +03:00

---
+++ songs/2.sql
@@ -0,0 +1,14 @@
+SELECT name, tempo FROM songs ORDER BY tempo;
++---------------------------------------------------------+---------+
+|                          name                           |  tempo  |
++---------------------------------------------------------+---------+
+| changes                                                 | 64.934  |
+| SAD!                                                    | 75.023  |
+| God's Plan                                              | 77.169  |
+| Feel It Still                                           | 79.028  |
+| Criminal                                                | 79.997  |
+| Lucid Dreams                                            | 83.903  |
+| Him & I (with Halsey)                                   | 87.908  |
+| Eastside (with Halsey & Khalid)                         | 89.391  |
+| River (feat. Ed Sheeran)                                | 90.09   |
+ ...
