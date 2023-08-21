312cb85594a6971dbb98701c25280a8c0e590454
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-21 15:41:31 +0000

    Mon, Aug 21, 2023, 6:41 PM +03:00

---
+++ songs/6.sql
@@ -0,0 +1,6 @@
+SELECT songs.name FROM songs JOIN artists.name ON 
+
+
+SELECT table1.column_name1, table1.column_name2, table2.column_name3
+FROM table1
+INNER JOIN table2 ON table1.common_column = table2.common_column;
