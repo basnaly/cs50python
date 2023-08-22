050a0e22832e1d853040fd4846d3594e13ec0a12
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 10:09:52 +0000

    Tue, Aug 22, 2023, 1:09 PM +03:00

---
+++ movies/1.sql
@@ -0,0 +1,14 @@
+
+
+
+SELECT title FROM movies LIMIT 5
+
++-----------------------------+
+|            title            |
++-----------------------------+
+| Tötet nicht mehr            |
+| Istoriya grazhdanskoy voyny |
+| La tierra de los toros      |
+| Dama de noche               |
+| El huésped del sevillano    |
++-----------------------------+
