473de30dc7cf27ec64ce849988744ba3bc2dfe8d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:13:52 +0000

    Tue, Aug 22, 2023, 2:13 PM +03:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,3 +2,9 @@ SELECT people.name, movies.title
    FROM people JOIN stars ON people.id = stars.person_id
    FROM movies JOIN start ON movies.id = stars.movie_id
    WHERE movies.title = 'Toy Story';
+
+
+SELECT people.name, movies.title
+   FROM people, movies JOIN stars
+   ON people.id = stars.person_id AND movies.id = stars.movie_id
+   WHERE movies.title = 'Toy Story';
