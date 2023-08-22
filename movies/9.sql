148c28a577ce2da68eabcc209123ed4b86e00d09
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 14:39:33 +0000

    Tue, Aug 22, 2023, 5:39 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT people.name
+SELECT COUNT(DISTINCT people.name)
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
