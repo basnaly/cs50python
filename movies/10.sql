c45a30ffbf4be398a06a55281727582abd06d452
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 14:44:44 +0000

    Tue, Aug 22, 2023, 5:44 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(DISTINCT people.name)
+SELECT DISTINCT people.name
    FROM people, ratings JOIN directors
    ON people.id = directors.person_id AND directors.movie_id = ratings.movie_id
    WHERE ratings.rating >= 9.0;
