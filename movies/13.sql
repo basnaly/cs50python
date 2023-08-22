fcccc6351e44ef2ea31eddace8ceca23815d0278
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 14:20:43 +0000

    Tue, Aug 22, 2023, 5:20 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,18 +1,33 @@
 SELECT DISTINCT people.name FROM
+
   (SELECT movies.id as id
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
-   WHERE people.name = 'Kevin Bacon') AS Kevin_movies, stars JOIN people
+   WHERE people.name = 'Kevin Bacon') AS Kevin_movies,
+
+   stars JOIN people
    ON people.id = stars.person_id
    AND Kevin_movies.id = stars.movie_id
    WHERE NOT people.name = 'Kevin Bacon' ORDER BY people.name;
 
 
-
-
-
-
-
-
- list the names of all people who starred in a movie in
- which Kevin Bacon also starred
++------------------------+
+|          name          |
++------------------------+
+| Air Supply             |
+| Alec Baldwin           |
+| Alison Lohman          |
+| Amanda Seyfried        |
+| Andreas Michera        |
+| Andy Garcia            |
+| Andy Peake             |
+| Anna Chlumsky          |
+| Anthony LaPaglia       |
+| Avery Tiiu Essex       |
+| Barbara Barrie         |
+| Ben Affleck            |
+| Bill Gerber            |
+| Bill Paxton            |
+| Bob Balaban            |
+| Bob Hoskins            |
+ ...
