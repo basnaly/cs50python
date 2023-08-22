81d9005803a64242f669bdca6425a041d9f7b32a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:35:50 +0000

    Tue, Aug 22, 2023, 2:35 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT people.name
-   FROM people, movies JOIN stars
+   FROM people, ratings JOIN directors
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
    ORDER BY people.birth;
