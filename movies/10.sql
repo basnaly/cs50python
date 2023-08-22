28f721b6827e04e1c2ce64b0e40d2aa84e122a79
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:40:41 +0000

    Tue, Aug 22, 2023, 2:40 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,7 +1,22 @@
+SELECT DISTINCT people.name
+   FROM people, ratings JOIN directors
+   ON people.id = directors.person_id AND directors.movie_id = ratings.movie_id
+   WHERE ratings.rating >= 9.0;
+
+
+
 SELECT DISTINCT people.name, ratings.rating
    FROM people, ratings JOIN directors
    ON people.id = directors.person_id AND directors.movie_id = ratings.movie_id
    WHERE ratings.rating >= 9.0;
 
-   list the names of all people who have directed a movie that
-   received a rating of at least 9.0.
+| Michael Kirk                         | 10.0   |
+| Kelly Rundle                         | 9.1    |
+| Stephen Dassatti                     | 9.4    |
+| Michalis Stavropoulos                | 9.4    |
+| Ron Bourke                           | 9.0    |
+| Aya Somech                           | 9.5    |
+| Randon Bopp                          | 9.4    |
+| Luigi Campi                          | 9.2    |
+| Orlando Rojas                        | 9.7    |
+| Joe Lujan                            | 9.6    |
