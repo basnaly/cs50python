14ff7f1d24f757ca2f8e311c5952a05ab928272a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 13:50:46 +0000

    Tue, Aug 22, 2023, 4:50 PM +03:00

---
+++ movies/13.sql
@@ -0,0 +1,12 @@
+SELECT movies.title
+   FROM people, movies JOIN stars
+   ON people.id = stars.person_id AND movies.id = stars.movie_id
+   WHERE people.name IN ('Bradley Cooper', 'Jennifer Lawrence');
+
+
+
+
+
+
+ list the names of all people who starred in a movie in
+ which Kevin Bacon also starred
