e3652ac8f349886fdb12be1862b61e226bcdec7a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:23:37 +0000

    Tue, Aug 22, 2023, 2:23 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,4 +2,4 @@ SELECT people.name
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
-   ORDER BY ;
+   ORDER BY people.birth;
