7535fb9ceea06d74df25e9556cb44e69775ea89c
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 10:13:21 +0000

    Tue, Aug 22, 2023, 1:13 PM +03:00

---
+++ movies/2.sql
@@ -0,0 +1,9 @@
+SELECT birth FROM people WHERE name = "Emma Stone";
+
+
+SELECT name, birth FROM people WHERE name = "Emma Stone";
++------------+-------+
+|    name    | birth |
++------------+-------+
+| Emma Stone | 1988  |
++------------+-------+
