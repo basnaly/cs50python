404d4affb49b49b06e547f499699d8cbdf240fce
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:44:57 +0000

    Tue, Aug 22, 2023, 2:44 PM +03:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT people.title, people.name
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE people.name = 'Chadwick Boseman'
-   ORDER BY people.birth;
+   ORDER BY people.rating;
 
 
 list the titles of the five highest rated movies (in order) that
