38b6aad108b11a96b9ec631bdb722b08bd55ae16
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:31:35 +0000

    Tue, Aug 22, 2023, 2:31 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
-SELECT people.name
+SELECT DISTINCT people.name
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
@@ -6,8 +6,33 @@ SELECT people.name
 
 
 
-SELECT people.name UNIQUE, people.birth
+SELECT DISTINCT people.name, people.birth
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
-   ORDER BY people.birth;
+   ORDER BY people.birth LIMIT 20;
+
+   +--------------------------+-------+
+|           name           | birth |
++--------------------------+-------+
+| Irela Bravo              | NULL  |
+| Jean Badin               | NULL  |
+| Amelia Barrett           | NULL  |
+| Sean Miller              | NULL  |
+| Jonny Spanish            | NULL  |
+| Julienne Hanzelka Kim    | NULL  |
+| G. Zachariah White       | NULL  |
+| Sam Dean                 | NULL  |
+| Sophia Dean              | NULL  |
+| Will Dean                | NULL  |
+| Kees Scholten            | NULL  |
+| Juan Francisco Rodríguez | NULL  |
+| David Burden             | NULL  |
+| Kathryn Carner           | NULL  |
+| Jerry Gelb               | NULL  |
+| Kirsten Hill             | NULL  |
+| Imma Villa               | NULL  |
+| Rosario Sparno           | NULL  |
+| Raffaele Di Florio       | NULL  |
+| David Nicksic            | NULL  |
++--------------------------+-------+
