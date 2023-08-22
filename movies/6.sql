e5779b4b7f350fff96e2d1ad31b5379281b456a1
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 10:41:43 +0000

    Tue, Aug 22, 2023, 1:41 PM +03:00

---
+++ movies/6.sql
@@ -0,0 +1,6 @@
+SELECT AVG(ratings.rating) FROM ratings JOIN movies ON movies.id = ratings.movie_id WHERE movies.year = 2012;
++---------------------+
+| AVG(ratings.rating) |
++---------------------+
+| 6.29787154592979    |
++---------------------+
