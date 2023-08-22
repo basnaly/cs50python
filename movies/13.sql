58a7eb7cb06458b677bc3888f0526423a4b0fe02
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 13:53:39 +0000

    Tue, Aug 22, 2023, 4:53 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
-SELECT people.name, movies.tirle
+SELECT people.name, movies.title
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE people.name = 'Kevin Bacon';
