3950df4e5eb25b669083d442c1ce8dbfa19ecf45
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:15:07 +0000

    Tue, Aug 22, 2023, 2:15 PM +03:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,6 +1,6 @@
 SELECT people.name, movies.title
-   FROM people JOIN stars ON people.id = stars.person_id
-   FROM movies JOIN start ON movies.id = stars.movie_id
+   FROM people, movies JOIN stars
+   ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.title = 'Toy Story';
 
 
@@ -8,3 +8,12 @@ SELECT people.name, movies.title
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.title = 'Toy Story';
+
++-------------+-----------+
+|    name     |   title   |
++-------------+-----------+
+| Tom Hanks   | Toy Story |
+| Tim Allen   | Toy Story |
+| Don Rickles | Toy Story |
+| Jim Varney  | Toy Story |
++-------------+-----------+
