7f9af75aaa428e765a99b5de8c464ac39e35b816
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 13:33:07 +0000

    Tue, Aug 22, 2023, 4:33 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT people.title, people.name
+SELECT movies.title, people.name
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
