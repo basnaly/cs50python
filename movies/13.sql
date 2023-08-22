11c45d95d5acb1a8efd26a9afcb7e6bf0900362f
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 14:14:46 +0000

    Tue, Aug 22, 2023, 5:14 PM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,9 +1,10 @@
 SELECT people.name FROM
-  (SELECT movies.id as Kevin_movies
+  (SELECT movies.id as id
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
-   WHERE people.name = 'Kevin Bacon'), stars JOIN people
+   WHERE people.name = 'Kevin Bacon') AS Kevin_movies, stars JOIN people
    ON people.id = stars.person_id
+   AND Kevin_movies.id = stars.movie_id;
 
 
 
