28ed4e2ca98f7f8231506e793dbe23a764849655
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 11:27:37 +0000

    Tue, Aug 22, 2023, 2:27 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -6,7 +6,7 @@ SELECT people.name
 
 
 
-SELECT people.name, people.birth
+SELECT people.name UNIQUE, people.birth
    FROM people, movies JOIN stars
    ON people.id = stars.person_id AND movies.id = stars.movie_id
    WHERE movies.year = 2004
