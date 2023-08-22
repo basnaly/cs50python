cbfbe4e19d3adcde1bd89aa71131d3371ad2f9ce
basnaly <89012819+basnaly@users.noreply.github.com>
2023-08-22 10:18:39 +0000

    Tue, Aug 22, 2023, 1:18 PM +03:00

---
+++ movies/3.sql
@@ -0,0 +1,14 @@
+SELECT title FROM movies WHERE year >= "2018";
+
+
+SELECT title, year FROM movies WHERE year >= "2018" LIMIT 5;
+
++----------------------------------------------------+------+
+|                       title                        | year |
++----------------------------------------------------+------+
+| Tötet nicht mehr                                   | 2019 |
+| Istoriya grazhdanskoy voyny                        | 2021 |
+| The Tango of the Widower and Its Distorting Mirror | 2020 |
+| The Other Side of the Wind                         | 2018 |
+| The Drive to Win                                   | 2019 |
++----------------------------------------------------+------+
