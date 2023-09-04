0da15a8f10c2ba846a53fce2ca6624fe7b410123
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-04 12:42:35 +0000

    Mon, Sep 4, 2023, 3:42 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -31,9 +31,6 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
-        if not name or not month or not day:
-            return render_template("index.html", error = "Fill all the fields")
-
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
