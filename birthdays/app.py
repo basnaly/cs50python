3765d12cdf51690e6d4782a0ac2f2ddabf0a9440
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-04 12:15:14 +0000

    Mon, Sep 4, 2023, 3:15 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -35,9 +35,7 @@ def index():
         #     printf("Fill all the fields")
 
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
-        # return redirect("/")
-
-        return render_template("index.html")
+        return redirect("/")
 
     else:
 
