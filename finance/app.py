42810cd3b261aa64860a53e9ebef4a695caaa995
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-06 10:48:12 +0000

    Wed, Sep 6, 2023, 1:48 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -112,13 +112,16 @@ def register():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        
+
         # Check for possible errors
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
         elif not request.form.get("password"):
             return apology("must provide password", 403)
+
+        elif request.form.get("password") != request.form.get("confirm_password"):
+            return apology("password does't match to confirm password", 403)
 
 
         # Insert the new user into users db
