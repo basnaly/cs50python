84d78046b0f49f0091d9505e9189ab23123aded4
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-06 11:29:20 +0000

    Wed, Sep 6, 2023, 2:29 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -128,7 +128,7 @@ def register():
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("confirmation does't match to password", 403)
 
-        elif request.form.get("password") != escape(s):
+        elif request.form.get("password") >= 3:
             return apology("the password doesn't match", 403)
 
         # Insert the new user into users db
