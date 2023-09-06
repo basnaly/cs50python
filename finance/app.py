82bb57b89effad5b2e5ffd096da5566dd101a7c0
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-06 14:51:02 +0000

    Wed, Sep 6, 2023, 5:51 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -140,11 +140,10 @@ def register():
         # Insert the new user into users db
         hash_user_password = generate_password_hash(request.form.get("password"))
 
-        new_user = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash_user_password)
-        print(new_user)
+        new_user_id = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash_user_password)
 
         # Remember which user has logged in
-        session["user_id"] = new_user["id"]
+        session["user_id"] = new_user_id
 
         # Redirect user to home page
         return redirect("/")
