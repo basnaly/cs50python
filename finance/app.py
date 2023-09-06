32d8b59108fff9f43c184bea0ce9234161402a77
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-06 14:06:26 +0000

    Wed, Sep 6, 2023, 5:06 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -159,3 +159,14 @@ def register():
 def sell():
     """Sell shares of stock"""
     return apology("TODO")
+
+
+
+# sqlite3 finance.db
+# .schema
+# CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                     username TEXT NOT NULL,
+#                     hash TEXT NOT NULL,
+#                     cash NUMERIC NOT NULL DEFAULT 10000.00);
+# CREATE TABLE sqlite_sequence(name,seq);
+# CREATE UNIQUE INDEX username ON users (username);
