dfcd351b7d44ba11c1eb3673f41b3a62a8db29fd
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-07 14:44:57 +0000

    Thu, Sep 7, 2023, 5:44 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 import math
-import datetime from datetime
+import datetime
 
 from helpers import apology, login_required, lookup, usd
 
@@ -109,11 +109,7 @@ def buy():
     else:
         db.execute("UPDATE portfolio_{user_id} SET shares = shares + VALUE(?)", shares)
 
-
     return redirect("/")
-
-
-
 
 @app.route("/history")
 @login_required
