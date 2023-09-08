96ba64653f1949c6f8b3176258d1e78cc088921e
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-08 10:57:58 +0000

    Fri, Sep 8, 2023, 1:57 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -99,8 +99,6 @@ def buy():
     else:
         db.execute(f"UPDATE portfolio_{user_id} SET shares = ?", int(row_symbol[0]["shares"]) + int(shares))
 
-    current_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
-
     updated_cash = db.execute("UPDATE cash FROM users SET cash = ? WHERE id = ?", current_cash["CASH"], user_id)
 
     return redirect("/")
