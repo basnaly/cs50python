f9611cd05c773996fd058d116e087f77bc62ca88
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-11 14:36:46 +0000

    Mon, Sep 11, 2023, 5:36 PM +03:00

--- project/app.py
+++ project/app.py
@@ -11,6 +11,28 @@ def lookup(symbol):
     start = end - datetime.timedelta(days=7)
 
     # Yahoo Finance API
+
+    curl --request POST
+    url = (
+        f'https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create' --header
+        f'x-api-key: sh428739766321522266746152871799' --data '
+        f {"query":{
+            "market":"UK",
+            "locale":"en-GB",
+            "currency":"GBP",
+            "query_legs":
+            [
+                {
+                    "origin_place_id":{"iata":"LHR"},
+                    "destination_place_id":{"iata":"SIN"},
+                    "date":{"year":2022,"month":12,"day":22}
+                }
+            ],
+            "adults":1,
+            "cabin_class":"CABIN_CLASS_ECONOMY"}
+            }'
+    )
+
     url = (
         f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
         f"?period1={int(start.timestamp())}"
