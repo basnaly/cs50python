a8efda3f08f73e9e27b29c2c0d4de8db86141ba2
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-12 15:36:45 +0000

    Tue, Sep 12, 2023, 6:36 PM +03:00

--- project/helpers.py
+++ project/helpers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import pytz
 import requests
 
@@ -52,7 +52,7 @@ def lookup(keyword):
 
         articles = response.json()['articles']
         for element in articles:
-            element["publishedAt"] = datetime(element["publishedAt"]).strftime("%Y-%m-%d %H:%M")
+            element["publishedAt"] = datetime.datetime(element["publishedAt"]).strftime("%Y-%m-%d %H:%M")
         return articles
 
     except (requests.RequestException, ValueError, KeyError, IndexError) as e:
