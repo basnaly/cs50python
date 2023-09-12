eebd9d8abf84aa64be2629cedff3246b65222c22
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-12 11:04:04 +0000

    Tue, Sep 12, 2023, 2:04 PM +03:00

--- project/app.py
+++ project/app.py
@@ -11,3 +11,26 @@ from helpers import lookup
 # Configure application
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+# @login_required
+def index():
+
+    
