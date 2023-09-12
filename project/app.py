a9500c11d8d3e6a0c4a8516a0f7ee5408b66797a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-12 11:00:25 +0000

    Tue, Sep 12, 2023, 2:00 PM +03:00

--- project/app.py
+++ project/app.py
@@ -1,61 +1,13 @@
-import datetime
-import pytz
-import requests
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask import redirect, render_template, session
+import math
+from datetime import datetime
 
-api_key = "c237f389d5674012bb0ca74e4f86675f"
+from helpers import lookup
 
-def lookup(query):
-    """Look up quote for symbol."""
+# Configure application
+app = Flask(__name__)
 
-
-
-    # Prepare API request
-    # symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=1)
-
-    # Yahoo Finance API
-
-    # GET https://newsapi.org/v2/everything?q=Apple&from=2023-09-12&sortBy=popularity&apiKey=API_KEY
-
-
-    # Query API
-    try:
-
-        url = ('https://newsapi.org/v2/everything?'
-        f'q={query}&'
-        f'from={start.strftime("%Y-%m-%d")}&'
-        'sortBy=popularity&'
-        f'apiKey={api_key}')
-
-        response = requests.get(url)
-
-        # print(response.json())
-
-        articles = response.json()['articles']
-        print(articles[0])
-
-    except (requests.RequestException, ValueError, KeyError, IndexError) as e:
-        print(e)
-        return None
-
-
-    # try:
-    #     response = requests.post(
-    #         url,
-    #         # cookies={"session": str(uuid.uuid4())},
-    #         headers = headers,
-    #         json = data
-    #     )
-    #     result_json = response.json()
-    #     session_token = result_json["sessionToken"]
-    #     print(session_token)
-    #     print(result_json["content"]["results"])
-
-
-    # except (requests.RequestException, ValueError, KeyError, IndexError) as e:
-    #     return None
-
-lookup("japan moon mission")
