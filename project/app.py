6e9d5e452b2712372fa0929336c0949eaf619f69
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-12 08:51:09 +0000

    Tue, Sep 12, 2023, 11:51 AM +03:00

--- project/app.py
+++ project/app.py
@@ -41,11 +41,6 @@ def lookup():
         print(session_token)
         print(result_json["content"]["results"])
 
-        # # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        # quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        # quotes.reverse()
-        # price = round(float(quotes[0]["Adj Close"]), 2)
-        # return {"name": symbol, "price": price, "symbol": symbol}
 
     except (requests.RequestException, ValueError, KeyError, IndexError) as e:
         return None
