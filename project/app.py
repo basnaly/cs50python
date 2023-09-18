e2606fc49b777577cbf641d2ef4702ee75cf2d4d
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-18 16:09:06 +0000

    Mon, Sep 18, 2023, 7:09 PM +03:00

--- project/app.py
+++ project/app.py
@@ -3,7 +3,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-import math
 from datetime import datetime
 
 from helpers import login_required, apology, lookup
