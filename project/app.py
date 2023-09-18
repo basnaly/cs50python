03363b5a1fb702c59f04026240a2277b6e25864a
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-18 10:31:05 +0000

    Mon, Sep 18, 2023, 1:31 PM +03:00

--- project/app.py
+++ project/app.py
@@ -420,6 +420,21 @@ def save_tags_to_article():
             id_tag,
             id_article,)
 
+    return redirect("/articles")
 
+@app.route("/delete-tags-from-article", methods=["GET"])
+@login_required
+def delete_tags_from_article():
+
+    user_id = session["user_id"]
+
+    id_article = request.args.get("id-article")
+    id_tag = request.args.get("id-tag")
+
+    user_saved_articles_table = f"articles_{user_id}"
+    update_list_articles = db.execute("UPDATE ? SET tag_id = ? WHERE id = ?",
+            user_saved_articles_table,
+            id_tag,
+            id_article,)
 
     return redirect("/articles")
