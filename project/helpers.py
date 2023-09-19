541373cf4bd171d2c8621a8dfb4d0808752968e1
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-19 15:25:30 +0000

    Tue, Sep 19, 2023, 6:25 PM +03:00

--- project/helpers.py
+++ project/helpers.py
@@ -11,24 +11,24 @@ api_key = "c237f389d5674012bb0ca74e4f86675f"
 def apology(message, code=400):
     """Render message as an apology to user."""
 
-    def escape(s):
-        """
-        Escape special characters.
+    # def escape(s):
+    #     """
+    #     Escape special characters.
 
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [
-            ("-", "--"),
-            (" ", "-"),
-            ("_", "__"),
-            ("?", "~q"),
-            ("%", "~p"),
-            ("#", "~h"),
-            ("/", "~s"),
-            ('"', "''"),
-        ]:
-            s = s.replace(old, new)
-        return s
+    #     https://github.com/jacebrowning/memegen#special-characters
+    #     """
+    #     for old, new in [
+    #         ("-", "--"),
+    #         (" ", "-"),
+    #         ("_", "__"),
+    #         ("?", "~q"),
+    #         ("%", "~p"),
+    #         ("#", "~h"),
+    #         ("/", "~s"),
+    #         ('"', "''"),
+    #     ]:
+    #         s = s.replace(old, new)
+    #     return s
 
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
