4b7600f58f2ceb4435e10dfacffbd0e4b60ea033
basnaly <89012819+basnaly@users.noreply.github.com>
2023-09-29 11:51:10 +0000

    Fri, Sep 29, 2023, 2:51 PM +03:00

--- project/app.py
+++ project/app.py
@@ -496,7 +496,7 @@ def filter_tags():
     user_saved_articles_table = f"articles_{user_id}"
     user_list_tags_table = f"tags_{user_id}"
     list_saved_articles = db.execute(
-        "SELECT *, ?.id as id FROM ? LEFT OUTER JOIN ? on ?.id = ?.tag_id ORDER BY published DESC;",
+        "SELECT *, ?.id as id FROM ? LEFT OUTER JOIN ? on ?.id = ?.tag_id WHERE ORDER BY published DESC;",
             user_saved_articles_table,
             user_saved_articles_table,
             user_list_tags_table,
