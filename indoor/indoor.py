e6a6b13fc3a18068df885c2cf03c5d876b7bd182
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-13 12:30:12 +0000

    Sat, Jan 13, 2024, 2:30 PM +02:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,7 +1,13 @@
+# define the main function
 def main():
+    # Ask user for prompt
     voice = input("")
+
+    # if user prompt typed lowercase string, print the string in upper case
     if voice.islower():
         print(voice.upper())
+
+    # if user prompt typed uppercase string, print the string in lower case
     else:
         print(voice.lower())
 
