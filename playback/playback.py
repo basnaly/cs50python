cd6778111779cdc3030418f187979264e1138b8e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-14 07:25:43 +0000

    Sun, Jan 14, 2024, 9:25 AM +02:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,5 +1,9 @@
+# Define the main function
 def main():
+    # Ask user type a sentence and convert it to a list
     list = input("Type a sentence ").split(' ')
+
+    # Convert the list to the 
     string = '...'.join(list)
     print(string)
 
