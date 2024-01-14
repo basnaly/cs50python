29b4996d316cd3955a02ad682079704742b9837e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-14 07:27:07 +0000

    Sun, Jan 14, 2024, 9:27 AM +02:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,9 +1,9 @@
 # Define the main function
 def main():
-    # Ask user type a sentence and convert it to a list
+    # Ask user to type a sentence and save it to a list variable
     list = input("Type a sentence ").split(' ')
 
-    # Convert the list to the 
+    # Convert the list to the string with
     string = '...'.join(list)
     print(string)
 
