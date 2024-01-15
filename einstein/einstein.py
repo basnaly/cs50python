1dce485730e6726baa70d59abd2f3fa8b68b5068
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-15 10:51:50 +0000

    Mon, Jan 15, 2024, 12:51 PM +02:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,9 +1,13 @@
-
-def energy(c):
+# Define energy function
+def energy():
+    # Prompt user to type a mass
     m = input("m: ")
+
+    # Calculate the energy using square function with argument of speed of light:
     e = m * square(300000000)
     print(f"E: {e}")
 
+# Define square function that return square of c
 def square(c):
     return c * c
 
