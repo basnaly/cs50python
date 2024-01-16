0773d12a82dfc5839b8286c418933b383105e8d0
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-16 09:05:33 +0000

    Tue, Jan 16, 2024, 11:05 AM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 def deep_func():
     print("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-    # Convert user's input without considering their case
+    # Convert user's input to an insensitively case
     answer = input("").casefold()
 
     if answer == "42" or answer == "forty-two" or answer == "forty two":
