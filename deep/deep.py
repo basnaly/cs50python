52410d3eb7ac6d49ec58620f604c6a0de9a2dc06
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-16 08:57:42 +0000

    Tue, Jan 16, 2024, 10:57 AM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -2,9 +2,10 @@ def deep_func():
     print("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
     answer = input("")
 
-    if answer == 42 or answer == "forty-two" or answer == "forty two":
+    if answer == "42" or answer == "forty-two" or answer == "forty two":
         print("Yes")
-    print("No")
+    else:
+        print("No")
 
 
 deep_func()
