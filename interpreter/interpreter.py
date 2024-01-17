bb47cfafddb6bcfac5132c3c1810718b7ebc03a2
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-17 09:50:30 +0000

    Wed, Jan 17, 2024, 11:50 AM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,18 +1,15 @@
 def main():
     expression = input("Expression: ").strip()
     x, y, z = expression.split(' ')
-    print(x)
-    print(y)
-    print(z)
     match y:
-        case y == '+':
-            print(int(x) + int(z))
-        case y == '-':
-            print(int(x) - int(z))
-        case y == '*':
-            print(int(x) * int(z))
-        case y == '/':
-            print(int(x) / int(z))
+        case'+':
+            print(float(x) + float(z))
+        case '-':
+            print(float(x) - float(z))
+        case '*':
+            print(float(x) * float(z))
+        case '/':
+            print(float(x) / float(z))
 
 
 main()
