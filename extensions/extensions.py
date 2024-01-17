a2ad03a361a9652913e4acd3d138cc583c12c5e6
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-17 08:59:08 +0000

    Wed, Jan 17, 2024, 10:59 AM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,7 +5,20 @@ def main():
         print('application/octet-stream')
 
     else:
-        match
+        name, extension = file.split('.')
+        match extension:
+            case 'gif':
+                print('image/gif')
+            case 'jpg'| 'jpeg':
+                print('image/jpeg')
+            case 'png':
+                print(application/'png')
+            case 'pdf':
+                print(application/'pdf')
+            case 'txt':
+                print(application/'txt')
+            case 'zip':
+                print(application/'zip')
 
 
 main()
