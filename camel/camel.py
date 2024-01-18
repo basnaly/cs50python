aa87dc50b4cc8a9f730644e6294187858b6ee66d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-18 09:29:24 +0000

    Thu, Jan 18, 2024, 11:29 AM +02:00

--- camel/camel.py
+++ camel/camel.py
@@ -5,6 +5,7 @@ def main():
         list.append(char)
         if char.isupper():
             char.replace(char, '_' + char.lower())
+            
             print('snake_case:' list.join())
         else:
             print(list.join())
