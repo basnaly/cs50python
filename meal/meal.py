f4b7e3ee1eaf7f03e73f4c13410858bc6c913507
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-18 08:41:23 +0000

    Thu, Jan 18, 2024, 10:41 AM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,24 @@
 def main():
-    cacs
-
-
+    digital_time = convert(time)
+    if 7 <= digital_time >= 8:
+        print('breakfast time')
+    elif 12 <= digital_time >= 13:
+        print('lunch time')
+    elif 18 <= digital_time >= 19:
+        print('dinner time')
+    else:
+        print('', end='')
 
 
 def convert(time):
-    sffsfs
+    time = input("What time is it? ")
+    hours, minutes = time.split(':')
+    converted_time = float(hours) + float(minuts) / 60
+    return converted_time
 
 
 
 if __name__ == "__main__":
+
 
 main()
