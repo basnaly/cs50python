466a1834b18bf67374fe3febd99938202e183f2c
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-18 08:53:47 +0000

    Thu, Jan 18, 2024, 10:53 AM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,19 +1,20 @@
 def main():
-    digital_time = convert(time)
-    if 7 <= digital_time >= 8:
+    time = input("What time is it? ")
+    digital_time = float(convert(time))
+    if 7 <= digital_time <= 8:
         print('breakfast time')
-    elif 12 <= digital_time >= 13:
+    elif 12 <= digital_time <= 13:
         print('lunch time')
-    elif 18 <= digital_time >= 19:
+    elif 18 <= digital_time <= 19:
         print('dinner time')
     else:
         print('', end='')
 
 
 def convert(time):
-    time = input("What time is it? ")
+
     hours, minutes = time.split(':')
-    converted_time = float(hours) + float(minuts) / 60
+    converted_time = float(hours) + float(minutes) / 60
     return converted_time
 
 
