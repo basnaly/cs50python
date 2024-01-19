f62dffdd76a1e2df13316ce9e28c5b3a2da249f8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 11:17:52 +0000

    Fri, Jan 19, 2024, 1:17 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,6 @@
 def main():
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    string = input('Input: ').casefold()
+    vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+    string = input('Input: ')
     for char in string:
         if char in vowels:
             string = string.replace(char, '')
