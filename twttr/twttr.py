12e6e008f4209757b731e60f011b99970443c2ee
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 11:12:10 +0000

    Fri, Jan 19, 2024, 1:12 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,6 @@
 def main():
     vowels = ['a', 'e', 'i', 'o', 'u']
     string = input('Input: ')
-    if string.isupper():
         for char in string:
             if char in vowels:
                 string = string.replace(char, '')
