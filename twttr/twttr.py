8e674a9e9906023adcab7f602de68b2ea6b116cb
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 10:42:04 +0000

    Fri, Jan 19, 2024, 12:42 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,8 +3,8 @@ def main():
     string = input('Input: ')
     for char in string:
         if char == 'a' and char == 'e' and char == 'i' and char == 'o' and char == 'u':
-            string = string.replace(char, '_')
-    print(f'Output: {string}')
+            string = string.replace(char, '-')
+            print(f'Output: {string}')
 
 
 
