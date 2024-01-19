946e28e60493a4eb1467ae264ced177bc8524148
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 11:49:31 +0000

    Fri, Jan 19, 2024, 1:49 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -5,9 +5,6 @@ def main():
         # Check if char is in the vowels list
         if char.lower() in vowels:
             string = string.replace(char, '')
-        # Check if char is in an uppercase in the string
-        # if char.isupper():
-        #     string = string.replace(char, char.upper())
     print(f'Output: {string}')
 
 
