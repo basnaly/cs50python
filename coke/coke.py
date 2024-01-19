c14e11f2868df42a56b3a352a189439f3af512d2
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 10:09:57 +0000

    Fri, Jan 19, 2024, 12:09 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,7 +3,7 @@ def main():
     amount_due = 50
     while amount_due > 0:
         print(f'Amount Due: {amount_due}')
-        coin = input('Insert Coin: ')
+        coin = int(input('Insert Coin: '))
         if coin != 25 and coin != 10 and coin != 5:
             continue
         else:
