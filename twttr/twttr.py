8f1820181377a778cd3acc85aac22734a46f43b0
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 11:21:01 +0000

    Fri, Jan 19, 2024, 1:21 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,8 +2,10 @@ def main():
     vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
     string = input('Input: ')
     for char in string:
+        # Check if char is in the vowels list
         if char in vowels:
             string = string.replace(char, '')
+        # Check if char is in an uppercase in the string
         if char.isupper():
             string = string.replace(char, char.upper())
     print(f'Output: {string}')
