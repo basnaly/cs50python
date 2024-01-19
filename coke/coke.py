77593ce6aead5e88e47c9b5e4eb3549351d41769
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 10:25:51 +0000

    Fri, Jan 19, 2024, 12:25 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,10 +3,12 @@ def main():
     amount_due = 50
     while amount_due > 0:
         print(f'Amount Due: {amount_due}')
+        # Get input from user and convert it to integer
         coin = int(input('Insert Coin: '))
         if coin != 25 and coin != 10 and coin != 5:
             continue
         else:
+            # Calculate remaining amount
             amount_due = amount_due - coin
     if amount_due < 0:
         print(f"Change Owed: {abs(amount_due)}")
