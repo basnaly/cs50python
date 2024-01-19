30cc20775f7e13211419ccb9ba41a08e22694b95
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 10:47:35 +0000

    Fri, Jan 19, 2024, 12:47 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,7 +2,7 @@ def main():
     # vowels = [a, e, i, o, u]
     string = input('Input: ')
     for char in string:
-        if char == 'a' or char == 'e' and char == 'i' and char == 'o' and char == 'u':
+        if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
             string = string.replace(char, '')
     print(f'Output: {string}')
 
