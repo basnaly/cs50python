c08ff7f8f1defef9a7447170e601a6a5d8cd049c
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 11:16:17 +0000

    Fri, Jan 19, 2024, 1:16 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -4,7 +4,8 @@ def main():
     for char in string:
         if char in vowels:
             string = string.replace(char, '')
-
+        if char.isupper():
+            string = string.replace(char, char.upper())
     print(f'Output: {string}')
 
 
