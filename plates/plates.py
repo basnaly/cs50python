41d32ce4d31744317f21b55f7bf831ed104fb5cf
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 12:33:25 +0000

    Fri, Jan 19, 2024, 2:33 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,9 +8,12 @@ def main():
 
 def is_valid(s):
 
-    is_letters = s[0:2].isalpha():
-
     is_letters_numbers = s.isalnum()
+    is_letters = s[0:2].isalpha():
+    min_letters =
+    max_letters =
+
+
 
 
 main()
