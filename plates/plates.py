9af83fc72e1e0b5ba6c7f14576c1174ff1da04d5
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-19 12:38:02 +0000

    Fri, Jan 19, 2024, 2:38 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,11 +8,14 @@ def main():
 
 def is_valid(s):
 
-    is_letters_numbers = s.isalnum()
-    is_letters = s[0:2].isalpha():
-    min_letters =
-    max_letters =
-
+    is_chars_numbers = s.isalnum()
+    is_chars = s[0:2].isalpha():
+    count = 0
+    for c in s:
+        if c.isalpha():
+            count = count + 1
+    if 2 < count <= 6
+    
 
 
 
