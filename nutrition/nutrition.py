bce29d01179588f86bca1e53631d3b9f9a8cf46e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-20 16:41:19 +0000

    Sat, Jan 20, 2024, 6:41 PM +02:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,6 +1,25 @@
 def main():
     dict = [
-        {}
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Avocado', 'calories': 50},
+        {'fruit': 'Banana', 'calories': 110},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
+        {'fruit': 'Apple', 'calories': 130},
     ]
 
 
