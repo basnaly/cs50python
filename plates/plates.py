6ab1ece834b2e9a24ed807b518d40c31cc7e4edd
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-20 07:04:15 +0000

    Sat, Jan 20, 2024, 9:04 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -12,7 +12,7 @@ def is_valid(s):
         return False
     if s[0:2].isalpha() == False:
         return False
-    if s[-1].isalpha() == False:
+    if s[-1].isalpha() == True:
         return False
     # count = 0
     # for c in s:
