b5b7475a6420ae3e586d481dfdd1958ce2644b27
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-20 06:51:04 +0000

    Sat, Jan 20, 2024, 8:51 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,16 +10,16 @@ def is_valid(s):
 
     if s.isalnum() == False:
         return False
-    if s[0:2].isalpha() == False:
-        return False
-    if s[3:-2].isdigit() == False:
-        return False
-    count = 0
-    for c in s:
-        if c.isalpha():
-            count = count + 1
-            if 2 > count > 6:
-                return False
+    # if s[0:2].isalpha() == False:
+    #     return False
+    # if s[3:-2].isdigit() == False:
+    #     return False
+    # count = 0
+    # for c in s:
+    #     if c.isalpha():
+    #         count = count + 1
+    #         if 2 > count > 6:
+    #             return False
     else:
         return True
 
