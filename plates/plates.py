0c2a10728d16432e64717d7430b6cfac3185454e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-20 08:13:25 +0000

    Sat, Jan 20, 2024, 10:13 AM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -12,13 +12,12 @@ def is_valid(s):
         return False
     if s[0:2].isalpha() == False:
         return False
-    if s[-2].isdigit and s[-1].isalpha() == True:
+    if s[-2].isdigit == True and s[-1].isalpha() == True:
         return False
     count = 0
     for c in s:
         if c.isalpha():
             count = count + 1
-    print(count)
     if count < 2 or count > 6:
         return False
     else:
