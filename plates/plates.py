b8648c99a327e0bd4bc6839fb7dc8acdf679359c
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-20 10:41:10 +0000

    Sat, Jan 20, 2024, 12:41 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,21 +7,28 @@ def main():
 
 
 def is_valid(s):
-
+    # Check if all chars are alphabetic and numeric only
     if s.isalnum() == False:
         return False
-    if s[0:2].isalpha() == False:
-        return False
-    if s[3:-2].isdigit == True and s[-1].isalpha() == True:
-        return False
-    count = 0
+    # Check if 2 first chars are alphabetic
+    if s[0:2].isalpha == False:
+        returm False
+    first_char = None
+    i = 0
+    # Check if the first char = 0
     for c in s:
-        if c.isalpha():
-            count = count + 1
-    if count < 2 or count > 6:
+        i += 1
+        if c.isdigit():
+            first_char == c
+            break
+    if first_char == 0:
         return False
-    else:
-        return True
+    # Check if between first_char and last char there is letter
+    if s[i:-1].is.digit == False:
+        return False
+    # 
+
+
 
 
 main()
