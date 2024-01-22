42d88bf478e90c0a839a890a847b94004afac816
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-22 15:51:03 +0000

    Mon, Jan 22, 2024, 5:51 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,34 +1,26 @@
 def main():
 
     while True:
-
-        # formatted_fraction = input('Fraction: ')
-        # if formatted_fraction == '4/4':
-        #     print('F')
-        #     return
-        # elif formatted_fraction == '0/1':
-        #     print('E')
-        #     return
-        # else:
-            try:
-                fraction = formatted_fraction.split('/')
-                if fraction[0].isdecimal() == False or fraction[1].isdecimal() == False:
-                    continue
-                if int(fraction[0]) > int(fraction[1]):
-                    continue
-                if fraction[0] == '0':
-                    print('E')
-                    return
-                if fraction[0] == fraction[1]
-                    print('F')
-                    return
-
-                result = round(float(fraction[0]) / float(fraction[1]) * 100, 1)
-                print(f'{int(result)}%')
+        formatted_fraction = input('Fraction: ')
+        try:
+            fraction = formatted_fraction.split('/')
+            if fraction[0].isdecimal() == False or fraction[1].isdecimal() == False:
+                continue
+            if int(fraction[0]) > int(fraction[1]):
+                continue
+            if fraction[0] == '0':
+                print('E')
                 return
-            except ValueError:
-                pass
-            except ZeroDivisionError:
+            if fraction[0] == fraction[1]:
+                print('F')
                 return
+
+            result = round(float(fraction[0]) / float(fraction[1]) * 100, 1)
+            print(f'{int(result)}%')
+            return
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            return
 
 main()
