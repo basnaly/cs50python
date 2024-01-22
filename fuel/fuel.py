c3b007631c67c8ab929c6bf6f2a153008c0277fe
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-22 15:48:02 +0000

    Mon, Jan 22, 2024, 5:48 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,22 +2,27 @@ def main():
 
     while True:
 
-        formatted_fraction = input('Fraction: ')
-        if formatted_fraction == '4/4':
-            print('F')
-            return
-        elif formatted_fraction == '0/1':
-            print('E')
-            return
-        else:
+        # formatted_fraction = input('Fraction: ')
+        # if formatted_fraction == '4/4':
+        #     print('F')
+        #     return
+        # elif formatted_fraction == '0/1':
+        #     print('E')
+        #     return
+        # else:
             try:
                 fraction = formatted_fraction.split('/')
                 if fraction[0].isdecimal() == False or fraction[1].isdecimal() == False:
                     continue
+                if int(fraction[0]) > int(fraction[1]):
+                    continue
                 if fraction[0] == '0':
                     print('E')
-                elif int(fraction[0]) > int(fraction[1]):
-                    continue
+                    return
+                if fraction[0] == fraction[1]
+                    print('F')
+                    return
+
                 result = round(float(fraction[0]) / float(fraction[1]) * 100, 1)
                 print(f'{int(result)}%')
                 return
