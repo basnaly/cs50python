faf3ca3a6d996cf04d34d8924c97656636a32d51
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-23 15:51:03 +0000

    Tue, Jan 23, 2024, 5:51 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,14 +6,17 @@ def main():
             # Convert user's input to insensitively case with uppercase chars
             item = input().casefold().upper()
             if item in dict_items:
+                # Add 1 to a count of the same word
                 dict_items[item] += 1
             else:
+                # Set
                 dict_items[item] = 1
             continue
 
         except EOFError:
             break
 
+    # Sort the dict alphabetically
     # From: https://www.freecodecamp.org/news/python-sort-dictionary-by-key/
     sorted_dict = dict(sorted(dict_items.items()))
     for item in sorted_dict:
