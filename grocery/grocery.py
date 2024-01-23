a22022e89928959eb97fdaa70dcb1c8e9d83af51
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-23 15:35:41 +0000

    Tue, Jan 23, 2024, 5:35 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,7 +3,12 @@ def main():
     while True:
         try:
             item = input().casefold().upper()
-            dict_items[item] = item
+            dict_items[item]
+            if item in dict_items:
+                dict_items[item] += 1
+            else:
+                dict_items[item] = 1
+
             print(dict_items)
 
         except EOFError:
