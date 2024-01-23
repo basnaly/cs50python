17a03e8c892d89eb776a4e474db60708de563fa8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-23 15:39:08 +0000

    Tue, Jan 23, 2024, 5:39 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,16 +3,17 @@ def main():
     while True:
         try:
             item = input().casefold().upper()
-            dict_items[item]
             if item in dict_items:
                 dict_items[item] += 1
             else:
                 dict_items[item] = 1
-
-            print(dict_items)
+            continue
 
         except EOFError:
             break
+
+    sorted_dict = dict(sorted(dict_items.item()))
+    print(sorted_dict)
 
 
 
