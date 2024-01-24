b22c5a299f09b82f3c65913984f0389c650e7788
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-24 17:14:28 +0000

    Wed, Jan 24, 2024, 7:14 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,50 +15,7 @@ MONTHES = [
 
 def main():
 
-    while True:
-        date = input('Date: ').strip()
-        try:
-            list = date.split('/')
-            if len(list) == 3: # 7/9/1234
-                year = list[2]
-
-                month = int(list[0])
-                if month > 12:
-                    continue
-                if month < 10:
-                    month = '0' + str(month)
-
-                day = int(list[1])
-                if day > 31:
-                    continue
-                if day < 10:
-                    day = '0' + str(day)
-
-            elif date[0].isalpha(): # September 8, 1636
-                list = date.split(' ')
-                year = list[2]
-
-                month = MONTHES.index(list[0]) + 1
-                if month > 12:
-                    continue
-                if month < 10:
-                    month = '0' + str(month)
-
-                day = list[1]
-                if len(day) ==  1:
-                    continue
-                day = int(list[1].replace(',', ''))
-                if day > 31:
-                    continue
-                if day < 10:
-                    day = '0' + str(day)
-            else:
-                continue
-
-            break
-
-        except ValueError:
-            continue
+    
 
     print(f'{year}-{month}-{day}')
 
