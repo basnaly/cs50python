26d29155a713158cd857188c98bd7e0499fbcac3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-24 16:20:29 +0000

    Wed, Jan 24, 2024, 6:20 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,7 +22,7 @@ def main():
             print(list)
             if len(list) == 3: # 7/9/1234
                 year = list[2]
-                print(f'{year}')
+                # print(f'{year}')
                 month = int(list[0])
                 if month < 10:
                     month = '0' + str(month)
@@ -30,7 +30,7 @@ def main():
                 day = int(list[1])
                 if day < 10:
                     day = '0' + str(day)
-            print(f'{year}-{month}-{day}')
+                # print(f'{year}-{month}-{day}')
             if date[0].isalpha(): # September 8, 1636
                 list = date.split(' ')
                 print(list)
