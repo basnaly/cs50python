989333f1616bf073a72d13069d5066d2a489ff91
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:15:54 +0000

    Thu, Jan 25, 2024, 2:15 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -7,12 +7,10 @@ def main():
             if len(list) == 1:
                 print(f'Output: {emoji.emojize(user_input)}')
             else:
-                for item in list:
-                    
-    user_text = user_input[0]
-    user_emoji = user_input[1]
-
-    #print(emoji.emojize(':camel:'))
+                index = list.index(':', start)
+                print(index)
+        except ValueError:
+            continue
 
 
 main()
