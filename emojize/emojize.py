cba0bc292a75f7a2341299b22776daf14849600d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:35:30 +0000

    Thu, Jan 25, 2024, 2:35 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,20 +1,13 @@
 import emoji
 
 def main():
-    while True:
-        list = input('Input: ').split(' ')
-        if len(list) == 1:
-            print(f'Output: {emoji.emojize(list[0])}')
-            break
-        else:
-            print(f'Output: {list[0]} {emoji.emojize(list[1])}')
+    list = input('Input: ').split(' ')
+    if len(list) == 1:
+        print(f'Output: {emoji.emojize(list[0])}')
 
-        # try:
-        #     if len(list) == 1:
-        #         print(f'Output: {emoji.emojize(user_input)}')
-
-        # except ValueError:
-        #     break
+    else:
+        print(f'Output: {list[0]} {emoji.emojize(list[1])}')
+    
 
 
 main()
