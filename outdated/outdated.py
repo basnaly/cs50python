f4f584c84d99054f657beeeb3ab98349b00c02fa
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 10:55:33 +0000

    Thu, Jan 25, 2024, 12:55 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,9 +18,25 @@ def main():
     while True:
         date = input('Date: ')
         try:
-            date_list = date.split('/')
+            date_list = date.split('/') #9/8/1636
             if len(date_list) == 3:
-                
+
+                year = date_list[2] # '1636'
+
+                month = int(date_list[0]) # 9
+                if month > 12:
+                    continue
+                if month < 10:
+                    month = '0' + str(month)
+
+                day = int(date_list[1]) # 8
+                if day > 31:
+                    continue
+                if day < 10:
+                    day = '0' + str(day)
+
+            elif 
+
 
         except ValueError:
             continue
