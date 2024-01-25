d8f46ffa1e2e25883e6e38ceb558f58921cee2d5
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:58:08 +0000

    Thu, Jan 25, 2024, 2:58 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,19 +1,6 @@
 import emoji
 
 def main():
-#     list = input('Input: ').split(' ')
-#     if len(list) == 1:
-#         print(f"Output: {emoji.emojize(list[0], language='alias')}")
-
-#     else:
-#         print(f"Output: {list[0]} {emoji.emojize(list[1], language='alias')}")
-
-#     index = None
-#     for item in list
-#         item.find()
-
-# ['abc', ':a:', ':c:', 'zx']
-
 
     string = input('Input: ')
     print(f"Output: {emoji.emojize(string ,language='alias')}")
