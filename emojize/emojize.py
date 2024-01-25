49a3e9e47e132e973a2fabd9a0d97b7cc2e977cc
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:06:43 +0000

    Thu, Jan 25, 2024, 2:06 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,7 +1,8 @@
 import emoji
 
 def main():
-    user_input = input('Input: ')
+    user_input = input('Input: ').split(' ')
+    user_text = 
     print(f'Output: {emoji.emojize(user_input)}')
     #print(emoji.emojize(':camel:'))
 
