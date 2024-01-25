51ee6f0eab655e2f41a84d914d466e2c5e8d19a3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:19:10 +0000

    Thu, Jan 25, 2024, 2:19 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -3,14 +3,14 @@ import emoji
 def main():
     while True:
         list = input('Input: ').split(' ')
+        index = list.index(':')
+        print(index)
         try:
             if len(list) == 1:
                 print(f'Output: {emoji.emojize(user_input)}')
-            else:
-                index = list.index(':')
-                print(index)
+
         except ValueError:
-            continue
+            break
 
 
 main()
