52ca8ce0277937d790a1a1002dd4aa92e0f3ef78
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:47:26 +0000

    Thu, Jan 25, 2024, 2:47 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -3,11 +3,11 @@ import emoji
 def main():
     list = input('Input: ').split(' ')
     if len(list) == 1:
-        print(f'Output: {emoji.emojize(list[0])}')
+        print(f"Output: {emoji.emojize(list[0], language='alias')}")
 
     else:
         print(f'Output: {list[0]} {emoji.emojize(list[1])}')
-    
+
 
 
 main()
