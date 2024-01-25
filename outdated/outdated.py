92433733fa58469ba5b74c27f252966c82bfcd36
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 11:22:22 +0000

    Thu, Jan 25, 2024, 1:22 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -36,7 +36,7 @@ def main():
                 if day < 10:
                     day = '0' + str(day)
 
-            elif en(date_list) == 1: # September 8, 1636
+            elif len(date_list) == 1: # September 8, 1636
                 print(date_list[0])
                 date_list = date.split(' ')
 
