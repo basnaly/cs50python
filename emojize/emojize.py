2ecab1f6c6b33242d463004c4e86aeb32071dbcf
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-25 12:51:29 +0000

    Thu, Jan 25, 2024, 2:51 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -6,8 +6,11 @@ def main():
         print(f"Output: {emoji.emojize(list[0], language='alias')}")
 
     else:
-        print(f'Output: {list[0]} {emoji.emojize(list[1])}')
+        print(f"Output: {list[0]} {emoji.emojize(list[1], language='alias')}")
 
+    index = None
+    for item in list
+        item.find()
 
-
+['abc', ':a:', ':c:', 'zx']
 main()
