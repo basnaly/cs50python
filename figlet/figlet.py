a34bf311de56b89fe7374ae2ad711aaf1b185de3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-26 12:51:45 +0000

    Fri, Jan 26, 2024, 2:51 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -17,7 +17,8 @@ elif len(sys.argv) == 3:
     if (sys.argv[1] != '-f' or sys.argv[1] != '--font') and sys.argv[2] not in fonts_list:
         sys.exit('Wrong arguments')
     else:
-        updated_font = figlet.setFont(font=sys.argv[2])
+        my_font = sys.argv[2]
+        updated_font = figlet.setFont(font=my_font)
         print(updated_font)
 
 else:
