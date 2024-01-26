8b61ecb87067866a98ec8fa27e1db55daae7c54f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-26 12:29:28 +0000

    Fri, Jan 26, 2024, 2:29 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,6 +8,7 @@ str = input('Input: ')
 
 if len(sys.argv) == 1:
     random_font = random.choices(figlet.getFonts())
+    set_font = figlet.setFont(font=random_font)
     print(random_font)
     # font = figlet.renderText(str)
     # print(font)
