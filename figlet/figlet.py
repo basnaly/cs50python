1082524d66834d710a2e3872bfd2880b2773cbb3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-26 12:56:37 +0000

    Fri, Jan 26, 2024, 2:56 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,9 +5,8 @@ import sys
 figlet = Figlet()
 fonts_list = figlet.getFonts()
 
-str = input('Input: ')
-
 if len(sys.argv) == 1:
+    str = input('Input: ')
     random_font = random.choices(figlet.getFonts())
     print(random_font)
     print(figlet.renderText(str))
@@ -16,6 +15,7 @@ elif len(sys.argv) == 3:
     if (sys.argv[1] != '-f' or sys.argv[1] != '--font') and sys.argv[2] not in fonts_list:
         sys.exit('Invalid usage')
     else:
+        str = input('Input: ')
         my_font = sys.argv[2]
         updated_font = figlet.setFont(font=my_font)
         print(figlet.renderText(str))
