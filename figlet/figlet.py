44c7e72761731013d1b7aa193db207b1caa0af6a
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-26 12:53:02 +0000

    Fri, Jan 26, 2024, 2:53 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -10,8 +10,7 @@ str = input('Input: ')
 if len(sys.argv) == 1:
     random_font = random.choices(figlet.getFonts())
     print(random_font)
-    font = figlet.renderText(str)
-    print(font)
+    print(figlet.renderText(str))
 
 elif len(sys.argv) == 3:
     if (sys.argv[1] != '-f' or sys.argv[1] != '--font') and sys.argv[2] not in fonts_list:
@@ -19,7 +18,7 @@ elif len(sys.argv) == 3:
     else:
         my_font = sys.argv[2]
         updated_font = figlet.setFont(font=my_font)
-        print(updated_font)
+        print(figlet.renderText(str))
 
 else:
     sys.exit('Wrong arguments')
