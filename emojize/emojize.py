6a30f9d6f3005a7627ba9beb7984d68f2887952c
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-26 11:55:53 +0000

    Fri, Jan 26, 2024, 1:55 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,8 +1,9 @@
 import emoji
 
 def main():
-    # Save user's
+    # Save user's input into str
     str = input('Input: ')
+    # From: https://pypi.org/project/emoji/
     print(f'Output: {emoji.emojize(str, language="alias")}')
 
 
