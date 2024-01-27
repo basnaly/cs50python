bf99bf8bdcf0a75defde213c38375bff6f162cfa
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-27 11:30:06 +0000

    Sat, Jan 27, 2024, 1:30 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,17 +1,5 @@
 import inflect
 
-p = inflect.engine()
-list = []
 
-while True:
-
-    try:
-        name = input('Name: ')
-        list.append(name)
-        l = p.join((list), final_sep="")
-        print("asd "+l)
-        continue
-    except EOFError:
-        break
 
 # "Adieu", "adieu", "to"
