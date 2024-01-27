e5716f97094e581f5dbc46800928ac85d1e2f924
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-27 10:41:30 +0000

    Sat, Jan 27, 2024, 12:41 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -3,15 +3,17 @@ import inflect
 p = inflect.engine()
 list = ["Adieu", "adieu", "to"]
 
-while True:
-    try:
-        name = input('Name: ')
-        print(f"{p.join((name), final_sep='')}")
+# while True:
+#     try:
+#         name = input('Name: ')
+#         print(f"{p.join((name), final_sep='')}")
 
-        continue
-    except EOFError:
-        break
+#         continue
+#     except EOFError:
+#         break
 
-
+name = input('Name:')
+list = p.join(name)
+print(list)
 
 
