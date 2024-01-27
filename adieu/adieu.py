1f541a629e972d7d3691304e5c4fbcd6e837dd20
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-27 10:58:22 +0000

    Sat, Jan 27, 2024, 12:58 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -6,7 +6,7 @@ list = ["Adieu", "adieu", "to"]
 while True:
     try:
         name = input('Name: ')
-        l = p.join((name))
+        l = p.join(name)
         print(l)
         continue
     except EOFError:
