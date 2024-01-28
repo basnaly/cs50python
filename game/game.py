d77a4b18597e83ce119e3d05eb0bb07b78da6e55
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-28 07:40:20 +0000

    Sun, Jan 28, 2024, 9:40 AM +02:00

--- game/game.py
+++ game/game.py
@@ -1,9 +1,23 @@
 import random
 
-while True:
-    level = input('Level: ')
-    if level < 1:
-        continue
-    else:
-        
+def main():
 
+    while True:
+
+        level = int(input('Level: '))
+        if level < 1:
+            continue
+        else:
+            random_number = random.randint(1, level)
+        guess = int(input('Guess: '))
+        if guess < 1:
+            continue
+        elif guess < random_number:
+            print('Too small!')
+        elif guess > random_number:
+            print('Too large!')
+        else:
+            print('Just right!')
+
+
+main()
