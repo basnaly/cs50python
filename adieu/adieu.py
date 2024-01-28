7081b5f0e80f755d86f5e6f84c93c07c847e04e8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-28 06:41:17 +0000

    Sun, Jan 28, 2024, 8:41 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,5 +1,22 @@
 import inflect
 
+p = inflect.engine()
+list = []
+
+def main():
+    while True:
+        name = input('Name: ')
+        try:
+            list.append(name)
+            list = p.join((name), final_sep="")
+            print(list)
+
+        except EOFError:
+            break
+
 
 
 # "Adieu", "adieu", "to"
+
+
+main()
