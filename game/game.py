75edca8d41ffb5a6598d25e48003c6a969cda3b7
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-28 08:01:23 +0000

    Sun, Jan 28, 2024, 10:01 AM +02:00

--- game/game.py
+++ game/game.py
@@ -9,7 +9,7 @@ def main():
         level = input('Level: ')
         if level.isdecimal() == False:
             continue
-        elif int(level) < 1:
+        elif int(level) < 0:
             continue
         else:
             random_number = random.randint(1, int(level))
@@ -22,7 +22,7 @@ def main():
         if guess.isdecimal() == False:
             continue
         guess = int(guess)
-        if guess < 1:
+        if guess < 0:
             continue
         elif guess < random_number:
             print('Too small!')
