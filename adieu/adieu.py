cd2c91f867f1e8c28691ff71d261783267a2ef31
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-28 06:51:26 +0000

    Sun, Jan 28, 2024, 8:51 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,12 +1,14 @@
 import inflect
 
 p = inflect.engine()
+# Create list of names
 names = []
 
 def main():
     while True:
         name = input('Name: ')
         try:
+            # Add name to the names list
             names.append(name)
             list = p.join((names), final_sep="")
             print(f'Adieu, adieu, to {list}')
