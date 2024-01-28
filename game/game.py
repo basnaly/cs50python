ea2f24623b9fd5985767018548ab5ca1a4d34161
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-28 08:31:54 +0000

    Sun, Jan 28, 2024, 10:31 AM +02:00

--- game/game.py
+++ game/game.py
@@ -2,19 +2,23 @@ import random
 
 while True:
 
+    # Prompt user for level
     level = input('Level: ')
+    # Check the user's input
     if level.isdecimal() == False:
         continue
     elif int(level) < 1:
         continue
     else:
+        # Generate random number
         random_number = random.randint(1, int(level))
         break
 
 while True:
 
+    # Prompt user for guess
     guess = input('Guess: ')
-
+    # Check the user's input
     if guess.isdecimal() == False:
         continue
     guess = int(guess)
