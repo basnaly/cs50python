ae5efb6bbdea4da829bfabb7bcfd705ea794f96c
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-29 12:27:01 +0000

    Mon, Jan 29, 2024, 2:27 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,6 +2,22 @@ import inflect
 
 p = inflect.engine()
 
+names = []
 
+def main():
+
+    while True:
+        try:
+            # Prompt user for name
+            name = input('Name: ')
+            # Add name into names list
+            names.append(name)
+            continue
+        except EOFError:
+            break
+
+    # Join names from names list to the str
+    str = p.join((names))
+    print(f'Adieu, adieu, to {str}')
 
 main()
---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,15 @@
+import requests
+import sys
+import json
+
+
+def main():
+    try:
+        if len(sys.argv) != 2:
+            
+
+    except requests.RequestException:
+        print("Error")
+
+
+main()
