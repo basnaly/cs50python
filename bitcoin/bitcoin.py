eb11443761c927ca8a8d4acbbca08eefbd3a1e6d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-29 12:29:34 +0000

    Mon, Jan 29, 2024, 2:29 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,10 +6,13 @@ import json
 def main():
     try:
         if len(sys.argv) != 2:
-            
+            sys.exit('Missing command-line argument')
+        elif sys.argv[1].isdigit() == False:
+            sys.exit('Command-line argument is not a number')
 
+        
     except requests.RequestException:
-        print("Error")
+        print('Error')
 
 
 main()
