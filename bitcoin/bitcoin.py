497cd601b028df953f9e745dc7cad6579f213dbe
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-29 13:05:08 +0000

    Mon, Jan 29, 2024, 3:05 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,27 +1,3 @@
-import requests
-import sys
-import json
-
-
-def main():
-    try:
-        if len(sys.argv) != 2:
-            sys.exit('Missing command-line argument')
-        try:
-            float(sys.argv[1])
-        except ValueError:
-            sys.exit('Command-line argument is not a number')
-
-        response = requests.get(
-            'https://api.coindesk.com/v1/bpi/currentprice.json'
-        )
-        data = response.json()
-        usd_rate = data['bpi']['USD']['rate_float']
-        result = float(usd_rate) * float(sys.argv[1])
-        print(f'${result:,.4f}')
-
-    except requests.RequestException:
-        print('Error')
 
 
 main()
