fecb5f9977ee631121ecc8388d41af6af9f671cf
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 12:10:51 +0000

    Tue, Jan 30, 2024, 2:10 PM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,7 +2,7 @@ from bank import value
 
 
 def test_uppercase_argument():
-    assert value('Hi') == '20'
+    assert value('Hi') == 20
 
 def test_punctuation_argument():
-    assert value('Hello, Sir') == '0'
+    assert value('Hello, Sir') == 0
