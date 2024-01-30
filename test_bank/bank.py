2f601eddd776864b079eb1205c3004e26d239c72
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 11:10:31 +0000

    Tue, Jan 30, 2024, 1:10 PM +02:00

---
+++ test_bank/bank.py
@@ -0,0 +1,14 @@
+def main():
+    greet = input('Greeting: ').strip().casefold().split(',')
+
+    # Check the first word in the sentence (if exists)
+    if greet[0] == 'hello':
+        print('$0')
+    # Check the first letter in the first word of the sentence
+    elif greet[0][0] == 'h':
+        print('$20')
+    else:
+        print('$100')
+
+
+main()
