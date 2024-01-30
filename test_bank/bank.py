a924a4720525d8f7255cd8f32d5f5309ef58708f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 11:20:18 +0000

    Tue, Jan 30, 2024, 1:20 PM +02:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,17 +1,20 @@
 def main():
     greet = input('Greeting: ').strip().casefold().split(',')
+    get_money = value(greet)
+    print(f'${get_money}')
 
-    # Check the first word in the sentence (if exists)
-    if greet[0] == 'hello':
-        print('$0')
-    # Check the first letter in the first word of the sentence
-    elif greet[0][0] == 'h':
-        print('$20')
-    else:
-        print('$100')
 
 
 def value(greeting):
+
+    # Check the first word in the sentence (if exists)
+    if greeting[0] == 'hello':
+        return 0
+    # Check the first letter in the first word of the sentence
+    elif greeting[0][0] == 'h':
+        return 20
+    else:
+        return 100
 
 
 if __name__ == '__main__':
