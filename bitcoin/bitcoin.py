cc83d2dfe5b10567eb17cd2b4beccfa8b622c9ec
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 08:50:17 +0000

    Tue, Jan 30, 2024, 10:50 AM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,6 +7,10 @@ def main():
 
     if len(sys.argv) != 2:
         print('Missing command-line argument ')
-    elif 
+    try:
+        elif float(sys.argv[1]):
+
+    except ValueError:
+        print('Command-line argument is not a number ')
 
 main()
