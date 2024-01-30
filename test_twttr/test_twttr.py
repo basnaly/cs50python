e85aa7694ab607e6cc2dd358d23f5bf371828368
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 10:57:01 +0000

    Tue, Jan 30, 2024, 12:57 PM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,8 +1,14 @@
 
 from twttr import shorten
 
-def test_argument():
+def test_lowercase_argument():
     assert shorten('about') == 'bt'
+
+def test_uppercase_argument():
     assert shorten('Room') == 'Rm'
+
+def test_number_argument():
     assert shorten('CS50python') == 'CS50pythn'
+
+def test_punctuation_argument():
     assert shorten('hello, world') == 'hll, wrld'
