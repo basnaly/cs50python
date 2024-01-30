e2ae45572ad44dec831f5506861c89f72d2f122f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 09:51:35 +0000

    Tue, Jan 30, 2024, 11:51 AM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,16 +1,11 @@
 def main():
-    word = input('Input: ')
-    string = shorten(word)
+    vowels = ['a', 'e', 'i', 'o','u']
+    string = input('Input: ')
+    for char in string:
+        # Check if char is in the vowels list
+        if char.lower() in vowels:
+            string = string.replace(char, '')
     print(f'Output: {string}')
 
 
-def shorten(word):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    for char in word:
-        # Check if char is in the vowels list
-        if char.lower() in vowels:
-            word = word.replace(char, '')
-    return word
-
-if __name__ == '__main__':
-    main()
+main()
