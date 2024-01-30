8de8bf864b3b7c2ed4fd410f32bc8648c82aa6a9
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 09:55:33 +0000

    Tue, Jan 30, 2024, 11:55 AM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,6 @@
-import pytest
 
 from twttr import shorten
 
 def test_argument():
     assert shorten('about') == 'bt'
-    assert shorten('twitter') != 'twitter'
+    #assert shorten('twitter') != 'twitter'
