f8b10addfeed5734fce87f979817aa1e55f366de
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-30 09:18:04 +0000

    Tue, Jan 30, 2024, 11:18 AM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,17 +1,16 @@
 def main():
-
-    
-    vowels = ['a', 'e', 'i', 'o','u']
-    string = input('Input: ')
-    for char in string:
-        # Check if char is in the vowels list
-        if char.lower() in vowels:
-            string = string.replace(char, '')
+    word = input('Input: ')
+    shorten(word)
     print(f'Output: {string}')
 
 
 def shorten(word):
-
+    vowels = ['a', 'e', 'i', 'o','u']
+    for char in word:
+        # Check if char is in the vowels list
+        if char.lower() in vowels:
+            string = string.replace(char, '')
+    return string
 
 if __name__ == '__main__':
     main()
