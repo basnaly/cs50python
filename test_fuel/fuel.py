dd40a39cfcf165ca684440ba6e95c8efa73df8c3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-31 15:14:14 +0000

    Wed, Jan 31, 2024, 5:14 PM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -6,28 +6,26 @@ def main():
             result = gauge(percentage)
             print(f'{result}')
         except ValueError:
-            pass
+            continue
         except ZeroDivisionError:
-            return
+            continue
 
 def convert(fraction):
 
-            # Split user's input by '/' and get list
-            fraction = fraction.split('/')
-            if fraction[0].isdecimal() == False or fraction[1].isdecimal() == False:
-                raise ValueError
-            if int(fraction[0]) > int(fraction[1]):
-                raise ValueError
-            if fraction[0] == '0':
-                return 'E'
-            if fraction[0] == fraction[1]:
-                return 'F'
+    # Split user's input by '/' and get list
+    fraction = fraction.split('/')
+    if fraction[0].isdecimal() == False or fraction[1].isdecimal() == False:
+        raise ValueError
+    if int(fraction[0]) > int(fraction[1]):
+        raise ValueError
+    if fraction[0] == '0':
+        return 'E'
+    if fraction[0] == fraction[1]:
+        return 'F'
 
-            # Calculate percent:
-            percentage = round(float(fraction[0]) / float(fraction[1]) * 100)
-            return percentage
-
-
+    # Calculate percent:
+    percentage = round(float(fraction[0]) / float(fraction[1]) * 100)
+    return percentage
 
 
 def gauge(percentage):
