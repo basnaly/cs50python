6152e03751e8105e068829be5af2358e429128f4
basnaly <89012819+basnaly@users.noreply.github.com>
2024-01-31 15:36:41 +0000

    Wed, Jan 31, 2024, 5:36 PM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,28 +1,3 @@
 import pytest
 from fuel import convert, gauge
 
-
-def test_correct_argument():
-    assert convert('3/4') == 75
-
-
-def test_not_numeric_argument():
-    with pytest.raises(ValueError):
-        convert('cat/dog')
-
-
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        convert('1/0')
-
-
-def test_full_argument():
-    assert gauge(int('99')) == 'F'
-
-
-def test_empty_argument():
-    assert gauge(int('1')) == 'E'
-
-
-def test_exist_persent():
-    assert gauge(50) == '50%'
