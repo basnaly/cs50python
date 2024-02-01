37f6a24afb06eaf9fc1b69da20bc964a65217c7a
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-01 11:26:29 +0000

    Thu, Feb 1, 2024, 1:26 PM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,5 +1,17 @@
 import pytest
+
 from fuel import convert, gauge
+
 
 def test_corrected_argument():
     assert convert('1/2') == 50
+
+
+def test_str_argument():
+    with pytest.raises(ValueError):
+        convert('cat/dog')
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert('1/0')
