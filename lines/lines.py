a911542781481fb55450c4415da13aa2f7a11af6
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-03 12:12:30 +0000

    Sat, Feb 3, 2024, 2:12 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -16,7 +16,7 @@ def main():
         try:
             with open(sys.argv[1]) as file:
                 for line in file:
-                    line.lstrip().startwith('#')
+                    line.startwith('#')
                     count += 1
                 print(count)
                 break
