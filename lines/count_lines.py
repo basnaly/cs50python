9cd41d3f16ea61708b044a6d2187b4d56f057704
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-03 11:53:44 +0000

    Sat, Feb 3, 2024, 1:53 PM +02:00

--- lines/count_lines.py
+++ lines/count_lines.py
@@ -1,1 +1,11 @@
-# 
+def main():
+    string = input("camelCase: ").strip()
+    # Loop on each char in user's string
+    for char in string:
+        # If there is an uppercase char in the string
+        if char.isupper():
+            string = string.replace(char, '_'+ char.lower())
+    print(f"snake_case: {string}")
+
+
+main()
