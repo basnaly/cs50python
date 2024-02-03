ea16335c06307b98bccc18604bc4358dd812657a
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-03 11:35:33 +0000

    Sat, Feb 3, 2024, 1:35 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,15 +1,17 @@
 import sys
 
 def main():
+    count = 0
     while True:
         if len(sys.argv) == 1:
             sys.exit('Too few command-line arguments')
         elif len(sys.argv) > 2:
             sys.exit('Too many command-line arguments')
         name, extention = sys.argv[1].split('.')
-        if extention != 'py':
-            sys.exit('Not a Python file')
+            if extention != 'py':
+                sys.exit('Not a Python file')
 
+        else:
 
 
 main()
