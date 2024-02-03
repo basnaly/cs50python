b015ff65bdac75668a59e9e7864442ab4d7c25f7
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-03 11:44:39 +0000

    Sat, Feb 3, 2024, 1:44 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -8,17 +8,17 @@ def main():
 
         elif len(sys.argv) > 2:
             sys.exit('Too many command-line arguments')
-            
-        name, extention = sys.argv[1].lstrip().split('.')
-            if extention != 'py':
-                sys.exit('Not a Python file')
+
+        # name, extention = sys.argv[1].lstrip().split('.')
+        #     if extention != 'py':
+        #         sys.exit('Not a Python file')
 
         else:
             try:
                 with open(sys.argv[1]) as file:
                     for line in file:
                         count += 1
-                    print count
+                    print(count)
             except FileNotFoundError:
                 sys.exit('File does not exist')
 
