b4badbb2386f2c132dc8963d542fb3b7b9698ed3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-04 17:17:18 +0000

    Sun, Feb 4, 2024, 7:17 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -21,12 +21,11 @@ def main():
                 print(row)
                 if i == 0:
                     headers = row
-                    name= headers[0]
-                    print(headers)
+                    name = headers[0]
                 else:
                     table.append({'name': row[name], 'large': row['large'], 'small': row['small']})
-                    i += 1
-                print(table)
+                i += 1
+            print(table)
                 # print(tabulate(name, large, small, headers, tablefmt='grid'))
     except FileNotFoundError:
         sys.exit('File does not exist')
