2ee5423a738c44774e3375ba4638fbb0b31d7a02
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-04 15:49:16 +0000

    Sun, Feb 4, 2024, 5:49 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,7 +2,13 @@ import sys
 from tabulate import tabulate
 
 def main():
-    if sys.argv
+    if len(sys.argv) < 2:
+        sys.exit('Too few command-line arguments')
+    elif len(sys.argv) > 2:
+        sys.exit('Too many command-line arguments')
+    name, extention = sys.argv[1].lstrip().split('.')
+    if extention != 'py':
+        sys.exit('Not a CSV file')
 
 
 
