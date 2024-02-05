aecacce344448bb2eff2b4eeb6e80ac3584fb94f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-05 11:08:36 +0000

    Mon, Feb 5, 2024, 1:08 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -15,18 +15,10 @@ def main():
     table = []
     try:
         with open(sys.argv[1]) as file:
-            reader = csv.reader(file)
-            i = 0
+            reader = csv.DictReader(file)
             for row in reader:
-                print(row)
-                if i == 0:
-                    headers = row
-                    name = headers[0]
-                else:
-                    table.append({'name': row[0], 'large': row[1], 'small': row[2]})
-                i += 1
-            print(table)
-                # print(tabulate(name, large, small, headers, tablefmt='grid'))
+                table.append(row)
+            print(tabulate(table, headers='keys', tablefmt="grid"))
     except FileNotFoundError:
         sys.exit('File does not exist')
 
