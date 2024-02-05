3790e2462b4f9018fe48cf5847d89c9c5591c123
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-05 12:30:11 +0000

    Mon, Feb 5, 2024, 2:30 PM +02:00

--- scourgify.py
+++ scourgify.py
@@ -1,1 +1,16 @@
+import sys
+import csv
 
+
+def main():
+
+    if len(sys.argv) < 3:
+        sys.exit('Too few command-line arguments')
+    elif len(sys.argv) > 3:
+        sys.exit('Too many command-line arguments')
+
+    try:
+
+
+
+main()
