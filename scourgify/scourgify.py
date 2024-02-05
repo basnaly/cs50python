1ff17c19656e7319d9f67ec848b88af374bbb5e8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-05 15:26:59 +0000

    Mon, Feb 5, 2024, 5:26 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -9,12 +9,14 @@ def main():
     elif len(sys.argv) > 3:
         sys.exit('Too many command-line arguments')
 
+    before = []
     try:
         with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
             for row in reader:
                 first, last = row['name'].split(',')
                 house = row['house']
+                before.append({'first': first, 'last': last, 'house': house})
 
         with open(sys.argv[2], 'a') as file:
             writer = csv.DictWriter(file, fieldnames=['first', 'last', 'house'])
