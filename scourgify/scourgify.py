fc7e2520252dc18d7bc6563fb33e52f3cde058e5
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-05 16:00:24 +0000

    Mon, Feb 5, 2024, 6:00 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,42 +1,7 @@
-import sys
-import csv
+import sys, csv
 
 
 def main():
-
-    if len(sys.argv) < 3:
-        sys.exit('Too few command-line arguments')
-    elif len(sys.argv) > 3:
-        sys.exit('Too many command-line arguments')
-
-    buffer = []
-    try:
-        with open(sys.argv[1]) as file_read:
-            with open(sys.argv[2], 'a') as file_write:
-                reader = csv.DictReader(file_read)
-                writer = csv.DictWriter(file_write, fieldnames=['first', 'last', 'house'])
-                writer.writeheader()
-                for row in reader:
-                    first, last = row['name'].split(',')
-                    house = row['house']
-                    writer.writerow({'first': first, 'last': last, 'house': house})
-
-
-        # with open(sys.argv[1]) as file:
-        #     reader = csv.DictReader(file)
-        #     for row in reader:
-        #         first, last = row['name'].split(',')
-        #         house = row['house']
-        #         buffer.append({'first': first, 'last': last, 'house': house})
-
-        # with open(sys.argv[2], 'a') as file:
-        #     writer = csv.DictWriter(file, fieldnames=['first', 'last', 'house'])
-        #     writer.writeheader()
-        #     for row in buffer:
-        #         writer.writerow(row)
-
-    except FileNotFoundError:
-        sys.exit('File does not exist')
 
 
 
