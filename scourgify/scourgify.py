ecd9aa6999e133b320a2f3d3a1adb40e398f61a8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-05 15:50:32 +0000

    Mon, Feb 5, 2024, 5:50 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -21,8 +21,8 @@ def main():
         with open(sys.argv[2], 'a') as file:
             writer = csv.DictWriter(file, fieldnames=['first', 'last', 'house'])
             writer.writeheader()
-            for row in file:
-                writer.writerow(before)
+            for row in before:
+                writer.writerow(row)
 
     except FileNotFoundError:
         sys.exit('File does not exist')
