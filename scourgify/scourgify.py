98b39abc7ab192862f4b4a153e2bbdccf39afcf0
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-06 12:27:19 +0000

    Tue, Feb 6, 2024, 2:27 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -13,6 +13,15 @@ def main():
         with open(sys.argv[1]) as reader_file:
             reader = csv.DictReader(reader_file)
             for row in reader:
+                first, last = row['name'].split(',')
+                house = row['house']
+                buffer.append({'first': first, 'last': last, 'house': house})
+
+        with open(sys.argv[2], 'w') as writer_file:
+            writer = csv.DictWriter(writer_file, fieldnames=['first', 'last', 'house'])
+            writer.writeheader()
+            for row in buffer:
+                writer.writerow(row)
 
     except FileNotFoundError:
         sys.exit(f'Could not read {sys.argv[1]} file')
