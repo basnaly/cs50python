cd8176249fe6af579621873b24af9e9468abf5ca
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-06 12:17:23 +0000

    Tue, Feb 6, 2024, 2:17 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,6 +3,19 @@ import sys, csv
 
 def main():
 
+    if len(sys.argv) < 3:
+        sys.exit('Too few command-line arguments ')
+    elif len(sys.argv) > 3:
+        sys.exit('Too many command-line arguments')
+
+    buffer = []
+    try:
+        with open(sys.argv[1]) as reader_file:
+            reader = csv.DictReader(reader_file)
+
+    except FileNotFoundError:
+        sys.exit(f'Could not read {sys.argv[1]} file')
+
 
 
 main()
