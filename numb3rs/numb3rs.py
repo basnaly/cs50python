f841e069d2413f105a42ea437cd95d0ed9520bed
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-10 10:12:29 +0000

    Sat, Feb 10, 2024, 12:12 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -12,7 +12,7 @@ def validate(ip):
     # 200-249 2[0-4][0-9]
     # 250-255 25[0-5]
 
-    regex_pattern = '([0-1]?[0-9]?[0-9]?)(2[0-4][0-9])(25[0-5])'
+    regex_pattern = '(25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]?)'
 
     if re.search(f"r'^{regex_pattern}\.{regex_pattern}\.{regex_pattern}\.{regex_pattern}$'", ip):
         return True
