653613be6216a41109c716ce67c06bfe911f96f4
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-10 10:24:02 +0000

    Sat, Feb 10, 2024, 12:24 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,1 +1,9 @@
-from numb3rs inport validate
+from numb3rs import validate
+
+
+def test_correct_ip():
+    assert validate('1.2.255.120') == True
+
+
+def test_incorrect_ip():
+    assert validate('3.120.256.128') == False
