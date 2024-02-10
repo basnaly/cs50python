2b13268c2dea4e37cb86e6d1640249609441fccb
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-10 18:14:57 +0000

    Sat, Feb 10, 2024, 8:14 PM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,9 +1,4 @@
 import re
-import sys
-
-
-# https://youtu.be/xvFZjo5PgG0
-# http://youtube.com/embed/xvFZjo5PgG0
 
 
 def main():
@@ -12,11 +7,7 @@ def main():
 
 def parse(s):
 
-    string = 'https://youtu.be/'
-    a = re.search(r'^(?:.+src=")(?:https?://)(?:(?:www\.)?youtube\.com/)(?:embed/)([a-zA-Z0-9]+)(?:".+)$', s)
-    if a is None:
-        return None
-    return string + a.group(1)
+
 
 
 if __name__ == "__main__":
