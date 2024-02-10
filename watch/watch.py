31fcec3871d33bbbc0c6922fe81b0506c17942a3
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-10 11:30:53 +0000

    Sat, Feb 10, 2024, 1:30 PM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -14,7 +14,7 @@ def parse(s):
 
     # url = re.search(r"^https?://(?:www\.)youtu.be[^\.com/embed]/(.+)$", s)
 
-    url = re.search(r"^.+(https?://).+$", s)
+    url = re.search(r"^(https?://).+$", s)
     return url.group(0)
 
 
