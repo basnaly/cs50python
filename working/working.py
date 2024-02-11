f2ffc1ef4adf6faedc26f7a9f65c994cef29c584
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-11 12:02:13 +0000

    Sun, Feb 11, 2024, 2:02 PM +02:00

--- working/working.py
+++ working/working.py
@@ -1,1 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    data = s.split(' ')
+    print(data)
+
+
+
+
+if __name__ == "__main__":
+    main()
 
