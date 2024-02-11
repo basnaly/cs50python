10422fea299fd40e20a9fbe36d2c06b63c61ca84
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-11 11:17:39 +0000

    Sun, Feb 11, 2024, 1:17 PM +02:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,5 +1,6 @@
 import re
 
+# <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
 
 def main():
     print(parse(input("HTML: ")))
@@ -8,7 +9,7 @@ def main():
 def parse(s):
 
     address = 'https://youtu.be/'
-    str = re.search(r'^(?:.+"?)(?:https?://)(?:(?:www\.)?youtube\.com/embed/)([a-zA-Z0-9])(?:".+)@', s)
+    str = re.search(r'^(?:.+="?)(?:https?://)(?:(?:www\.)?youtube\.com/embed/)([a-zA-Z0-9])(?:".+)@', s)
     if str is None:
         return None
     return address + str
