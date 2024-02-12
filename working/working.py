71b2d10574608493a07e1f932e19e9868ffccc0a
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-12 13:54:06 +0000

    Mon, Feb 12, 2024, 3:54 PM +02:00

--- working/working.py
+++ working/working.py
@@ -10,6 +10,7 @@ def convert(s):
     try:
 
         # 9:00 AM to 5:00 PM
+        # 09:00 AM to 5:00 PM
         # 24: 2[0-4] 1[0-9] [0-9]
         hours = r'2[0-4]|1[0-9]|[0-9]'
         minutes = r'[0-59]'
@@ -19,7 +20,8 @@ def convert(s):
         print(start_hour, start_minutes, am, end_hour, end_minutes, pm)
 
         if am == 'AM':
-            
+            start_time = start_hour + ':' + start_minutes
+        print(start_time)
 
     except:
         raise ValueError
