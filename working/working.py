6b44bea8a2ebe51253b46032f84ada9f7c45217f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-12 14:16:39 +0000

    Mon, Feb 12, 2024, 4:16 PM +02:00

--- working/working.py
+++ working/working.py
@@ -23,6 +23,11 @@ def convert(s):
         start_time = start_hour + start_minutes
         end_time = end_hour + end_minutes
 
+        if start_minutes is None:
+            start_minutes == ''
+        if end_minutes is None:
+            end_minutes == ''
+
         if am.lstrip() == 'AM' and int(start_hour) < 10:
             start_time = '0' + start_hour + start_minutes
         print(start_time)
