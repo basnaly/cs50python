c4194513f5b753f12cc3b6a3e03e84f3f7517705
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-12 12:46:36 +0000

    Mon, Feb 12, 2024, 2:46 PM +02:00

--- working/working.py
+++ working/working.py
@@ -8,11 +8,14 @@ def main():
 
 def convert(s):
     try:
+
+        # 9:00 AM to 5:00 PM
         # 24: 2[0-4] 1[0-9] [0-9]
         hours = r'2[0-4]|1[0-9]|[0-9]'
         minutes = r'[0-59]'
 
-        a = re.search(fr'^({hours})(:{minutes})$', s)
+        a = re.search(fr'^({hours})$', s)
+        # a = re.search(fr'^({hours})(:{minutes})$', s)
         # start_hour, start_minutes
         # print(start_hour, start_minutes)
         print(a.group(1))
