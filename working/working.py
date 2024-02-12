5e6189ffba4f000039d22609859455cee821290e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-12 17:43:36 +0000

    Mon, Feb 12, 2024, 7:43 PM +02:00

--- working/working.py
+++ working/working.py
@@ -17,7 +17,7 @@ def convert(s):
         hours = r'2[0-4]|1[0-9]|[0-9]'
         minutes = r'[0-5][0-9]'
 
-        data = re.search(fr'^({hours})(:{minutes})?( [A|P]M)(?: to )({hours})(:{minutes})?( [A|P]M)$', s)
+        data = re.search(fr'^({hours})(:{minutes})?( [A|P]M)(?: to )?({hours})(:{minutes})?( [A|P]M)$', s)
         start_hour, start_minutes, start_format, end_hour, end_minutes, end_format = data.groups(':00')
         # print(start_hour, start_minutes, am, end_hour, end_minutes, pm)
         # 9 :00  AM 5 :00  PM
