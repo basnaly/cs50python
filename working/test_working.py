576aad566d5287a1a6d4f4bb59c0ff42447f2669
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-13 10:48:02 +0000

    Tue, Feb 13, 2024, 12:48 PM +02:00

--- working/test_working.py
+++ working/test_working.py
@@ -2,20 +2,40 @@ from working import convert
 import pytest
 
 
-def test_correct_version():
-    assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
+# def test_correct_version():
+#     assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
+
+
+# def test_incorrect_version():
+#     with pytest.raises(ValueError):
+#         convert('9:60 AM to 5:60 PM')
+
+
+# def test_omit_minutes():
+#     with pytest.raises(ValueError):
+#         convert('9:00 AM to 5: PM')
+
+
+# def test_omit_to():
+#     with pytest.raises(ValueError):
+#         convert('9:00 AM 5:20 PM')
+
+
+
+def test_corrected_version():
+    assert convert('8 AM to 7 PM') == '08:00 to 19:00'
 
 
 def test_incorrect_version():
     with pytest.raises(ValueError):
-        convert('9:60 AM to 5:60 PM')
+        convert('7:20 AM to 8:60 PM')
 
 
 def test_omit_minutes():
     with pytest.raises(ValueError):
-        convert('9:00 AM to 5: PM')
+        convert('10:15 AM to 5: PM')
 
 
 def test_omit_to():
     with pytest.raises(ValueError):
-        convert('9:00 AM 5:20 PM')
+        convert('11:55 AM - 6:40 PM')
--- working/working.py
+++ working/working.py
@@ -9,41 +9,72 @@ def main():
 def convert(s):
     try:
 
-        # 9:00 AM to 5:00 PM
-        # 09:00 AM to 5:00 PM
+    #     # 9:00 AM to 5:00 PM
+    #     # 09:00 AM to 5:00 PM
 
-        # 24: 20->24: 2[0-4] 10->19: 1[0-9] 0->9: [0-9]
-        # 59: 0->9: [0-9] 10->59: [15][09]
-        hours = r'2[0-4]|1[0-9]|[0-9]'
+    #     # 24: 20->24: 2[0-4] 10->19: 1[0-9] 0->9: [0-9]
+    #     # 59: 0->9: [0-9] 10->59: [15][09]
+    #     hours = r'2[0-4]|1[0-9]|[0-9]'
+    #     minutes = r'[0-5][0-9]'
+
+    #     data = re.search(fr'^({hours})(:{minutes})?( [A|P]M)(?: to)? ({hours})(:{minutes})?( [A|P]M)$', s)
+    #     start_hour, start_minutes, start_format, end_hour, end_minutes, end_format = data.groups(':00')
+    #     # print(start_hour, start_minutes, am, end_hour, end_minutes, pm)
+    #     # 9 :00  AM 5 :00  PM
+
+    #     start_time = start_hour + start_minutes
+    #     end_time = end_hour + end_minutes
+
+    #     if start_format.lstrip() == 'AM' and int(start_hour) < 10:
+    #         start_time = '0' + start_hour + start_minutes
+
+    #     if start_format.lstrip() == 'AM' and (start_hour == '12:00' or start_hour == '12'):
+    #         start_time = '00:00'
+
+    #     if start_format.lstrip() == 'PM':
+    #         start_time = str(int(start_hour) + 12) + start_minutes
+
+    #     if end_format.lstrip() == 'PM':
+    #         end_time = str(int(end_hour) + 12) + end_minutes
+
+    #     if end_format.lstrip() == 'AM' and int(end_hour) < 10:
+    #         end_time = '0' + end_hour + end_minutes
+
+    #     if end_format.lstrip() == 'PM' and (end_hour == '12:00' or end_hour == '12'):
+    #         end_time = '12:00'
+
+    #     return start_time + ' to ' + end_time
+
+    # except:
+    #     raise ValueError
+
+
+
+    try:
+        hour = r'[0-9]|1[0-9]|2[0-4]'
         minutes = r'[0-5][0-9]'
+        format = r' [A|P]M'
 
-        data = re.search(fr'^({hours})(:{minutes})?( [A|P]M)(?: to)? ({hours})(:{minutes})?( [A|P]M)$', s)
+        data = re.search(fr'^({hour})(:{minutes})?({format})(?: to )({hour})(:{minutes})?({format})?$', s)
         start_hour, start_minutes, start_format, end_hour, end_minutes, end_format = data.groups(':00')
-        # print(start_hour, start_minutes, am, end_hour, end_minutes, pm)
-        # 9 :00  AM 5 :00  PM
-
-        start_time = start_hour + start_minutes
-        end_time = end_hour + end_minutes
+        # print(start_hour, start_minutes, start_format, end_hour, end_minutes, end_format)
 
         if start_format.lstrip() == 'AM' and int(start_hour) < 10:
-            start_time = '0' + start_hour + start_minutes
-
-        if start_format.lstrip() == 'AM' and (start_hour == '12:00' or start_hour == '12'):
-            start_time = '00:00'
-
-        if start_format.lstrip() == 'PM':
-            start_time = str(int(start_hour) + 12) + start_minutes
-
-        if end_format.lstrip() == 'PM':
-            end_time = str(int(end_hour) + 12) + end_minutes
+            start_hour = '0' + start_hour
 
         if end_format.lstrip() == 'AM' and int(end_hour) < 10:
-            end_time = '0' + end_hour + end_minutes
+            end_hour = '0' + end_hour
 
-        if end_format.lstrip() == 'PM' and (end_hour == '12:00' or end_hour == '12'):
-            end_time = '12:00'
+        if start_format.lstrip() == 'AM' and int(start_hour) == 12:
+            start_hour = '00:00'
 
-        return start_time + ' to ' + end_time
+        if start_format.lstrip() == 'PM':
+            start_hour = str(int(start_hour) + 12)
+
+        if end_format.lstrip() == 'PM':
+            end_hour = str(int(end_hour) + 12)
+
+        return start_hour + start_minutes + ' to ' + end_hour + end_minutes
 
     except:
         raise ValueError
