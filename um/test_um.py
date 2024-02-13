26697394ac02eddeef07e9b432bbea929c7fdb36
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-13 15:25:43 +0000

    Tue, Feb 13, 2024, 5:25 PM +02:00

---
+++ um/test_um.py
@@ -0,0 +1,14 @@
+from um import count
+import pytest
+
+
+def test_correct_argument():
+    assert count('today, um, is, cold, um') == 2
+
+
+def test_yum_argument():
+    assert count('today, um, is, cold, yum') == 1
+
+
+def test_dots_argument():
+    assert count('um...') == 1
