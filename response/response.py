869910662effc2462d2533c8243f2fc8787d6f13
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-14 10:41:04 +0000

    Wed, Feb 14, 2024, 12:41 PM +02:00

--- response/response.py
+++ response/response.py
@@ -1,7 +1,16 @@
+from validator_collection import validators, checkers, errors
+import validators
 
 
 def main():
 
+    email = input("What's your email address?")
+
+    is_valid = validators.email()
+    if is_valid:
+        print('Valid')
+    else:
+        print('Invalid')
 
 
 main()
