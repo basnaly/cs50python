23c47f5520e5a010130f3622784b7d2e1ba66d53
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-18 15:30:48 +0000

    Sun, Feb 18, 2024, 5:30 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -7,8 +7,8 @@ def main():
     minutes = get_minutes()
 
     p = inflect.engine()
-    # result = p.number_to_words(minutes).capitalize()
-    # print(f'{result} minutes.')
+    result = p.number_to_words(minutes).capitalize()
+    print(f'{result} minutes.')
 
 
 def get_minutes():
@@ -22,7 +22,7 @@ def get_minutes():
         print(bithday_date)
 
         days = today - bithday_date
-        total_minutes = timedelta.total_seconds(days) / 60
+        total_minutes = int(timedelta.total_seconds(days) / 60)
         print(total_minutes)
         # return total_minutes
 
