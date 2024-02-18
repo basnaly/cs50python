7198250f2fee51350081c0e325e75665a6b1d993
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-18 12:27:44 +0000

    Sun, Feb 18, 2024, 2:27 PM +02:00

--- seasons/test_sescons.py
+++ seasons/test_sescons.py
@@ -3,8 +3,14 @@ import inflect
 
 
 def main():
-    minutes = get_minutes()
+    minutes = get_minutes(input('Date of Birth: '))
 
     p = inflect.engine()
-    result = 
+    result = p.number_to_words(minutes).capitalize()
+    print(f'{result} minutes.')
 
+
+def get_minutes():
+
+
+main()
