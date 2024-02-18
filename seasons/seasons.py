ef96252832451735288d33e4c1e614cc827e4de8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-18 15:41:01 +0000

    Sun, Feb 18, 2024, 5:41 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,31 +1,30 @@
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 import inflect
 import sys
 
 
 def main():
     minutes = get_minutes()
-    print(minutes)
 
     p = inflect.engine()
-    result = p.number_to_words(1234).capitalize()
+    result = p.number_to_words(minutes).capitalize()
     print(f'{result} minutes.')
 
 
 def get_minutes():
 
     try:
-
         today = date.today()
         bithday = input('Date of Birth: ')
-        year, month, day = bithday.split('-')
-        bithday_date = date(int(year), int(month), int(day))
-        print(bithday_date)
 
+        year, month, day = bithday.split('-')
+        # Transform bithday into date format
+        bithday_date = date(int(year), int(month), int(day))
+
+        # Calculate days between today and bithday
         days = today - bithday_date
         total_minutes = int(timedelta.total_seconds(days) / 60)
-        print(total_minutes)
-        # return total_minutes
+        return total_minutes
 
     except ValueError:
         sys.exit('Invalid date')
