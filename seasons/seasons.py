9fbf8b547a00ebbc8ee3d147e76484362f62e3af
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-18 12:41:23 +0000

    Sun, Feb 18, 2024, 2:41 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -6,17 +6,19 @@ def main():
     minutes = get_minutes()
 
     p = inflect.engine()
-    result = p.number_to_words(minutes).capitalize()
-    print(f'{result} minutes.')
+    # result = p.number_to_words(minutes).capitalize()
+    # print(f'{result} minutes.')
 
 
 def get_minutes():
 
-    today = date.
+    today = date.today()
+    today_formated = date.fromisoformat(today)
+    print(today_formated)
 
-    bithday = input('Date of Birth: ')
-    bithday_formatted =
-    except
+    # bithday = input('Date of Birth: ')
+    # bithday_formatted =
+
 
 
 main()
