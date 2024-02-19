031aea76482d0b15c18d8fbc72fd093e7700870e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-19 11:07:31 +0000

    Mon, Feb 19, 2024, 1:07 PM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,2 +1,26 @@
 
-class Jar
+class Jar:
+    def __init__(self, capacity=12):
+        ...
+
+    def __str__(self):
+        ...
+
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
+
+    
