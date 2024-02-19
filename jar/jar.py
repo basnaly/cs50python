4d839bf3fcaa6ba88fe04ccde7022870059706bb
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-19 11:20:49 +0000

    Mon, Feb 19, 2024, 1:20 PM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -18,11 +18,11 @@ class Jar:
 
     @property
     def capacity(self):
-        ...
+        return self._capacity
 
     @property
     def size(self):
-        ...
+        return self._size
 
 
 
