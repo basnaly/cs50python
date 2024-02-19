052cc847fca00057ddcad99f7e05d0b5fe34cda7
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-19 11:19:02 +0000

    Mon, Feb 19, 2024, 1:19 PM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,10 +1,12 @@
 
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        self.capacity = capacity
+        if capacity < 0:
+            raise ValueError('Capacity cannot be negative')
 
     def __str__(self):
-        ...
+        return f'{self.capacity}'
 
 
     def deposit(self, n):
@@ -23,4 +25,4 @@ class Jar:
         ...
 
 
-    
+
