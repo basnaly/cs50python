310d5f0f6f39a269f694e24220288607f8c4cbd8
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-20 10:48:42 +0000

    Tue, Feb 20, 2024, 12:48 PM +02:00

---
+++ jar/test_jar.py
@@ -0,0 +1,22 @@
+from jar import Jar
+
+
+def test_init():
+    ...
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ''
+    jar.deposit(2)
+    assert str(jar) == '🍪🍪'
+    jar.deposit(6)
+    assert str(jar) == '🍪🍪🍪🍪🍪🍪'
+
+
+def test_deposit():
+    ...
+
+
+def test_withdraw():
+    ...
