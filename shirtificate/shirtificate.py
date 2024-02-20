16d7b5de8a8e01c7f71afbc24c8104f75f5660b6
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-20 11:45:54 +0000

    Tue, Feb 20, 2024, 1:45 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,8 +1,22 @@
 from fpdf import FPDF
 
 
+class PDF(FPDF):
+    def header(self):
+        # Set font
+        self.set_font('arial', 'B', 20)
+        # Move cursor to the right:
+        self.cell(80)
+
+        # Print title
+        self.cell(30, 10, 'CS50 Shirtificate', border=1, align='C')
+        # perform a line break:
+        self.ln(20)
+
+
+
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font('arial', 'B', 18)
-pdf.cell(40, 10, 'Abc')
+# pdf.set_font('arial', 'B', 18)
+# pdf.cell(40, 10, 'Abc')
 pdf.output('shirtificate.pdf')
