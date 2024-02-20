e8c29619cd60a083fe1737972f9a4d898b2cdf07
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-20 17:26:14 +0000

    Tue, Feb 20, 2024, 7:26 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,6 +6,6 @@ pdf.add_page()
 pdf.image('shirtificate.png', 20, 80, 170, keep_aspect_ratio=True)
 
 pdf.set_font('helvetica', 'B', 40)
-pdf.t_margin = 100
-pdf.cell(text='CS50 Shirtificate', new_x="LMARGIN", new_y="TMARGIN", center=True)
+pdf.y = 40
+pdf.cell(text='CS50 Shirtificate', new_x="LMARGIN", new_y="TOP", center=True)
 pdf.output('shirtificate.pdf')
