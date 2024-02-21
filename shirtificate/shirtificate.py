75ea0cb610d484807a5741cc53ace0e5c909d92d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-21 16:12:42 +0000

    Wed, Feb 21, 2024, 6:12 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,11 +1,15 @@
 from fpdf import FPDF
 
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.add_page()
-pdf.image('shirtificate.png', 20, 80, 170, keep_aspect_ratio=True)
+def main():
+    name = input('Name: ')
 
-pdf.set_font('helvetica', 'B', 40)
-pdf.y = 40
-pdf.cell(text='CS50 Shirtificate', new_x="LMARGIN", new_y="TOP", center=True)
-pdf.output('shirtificate.pdf')
+
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.image(30, 10 name='shirtificate.png', keep_aspect_ratio=True)
+
+    pdf.set_font(family='helvetica', style='', size=40)
+    pdf.cell()
+
+    pdf.output('shirtificate.pdf')
