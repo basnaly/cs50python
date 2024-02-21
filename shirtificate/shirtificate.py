0f55cc7719a31cbe27a327f51c53931e35919fe7
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-21 16:18:14 +0000

    Wed, Feb 21, 2024, 6:18 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,10 +7,15 @@ def main():
 
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
-    pdf.image(30, 10 name='shirtificate.png', keep_aspect_ratio=True)
+    pdf.image(30, 10, name='shirtificate.png', keep_aspect_ratio=True)
 
     pdf.set_font(family='helvetica', style='', size=40)
     pdf.y = 40
+    pdf.cell(text='CS50 Shirtificate', center=True, new_y='TOP')
+
+    pdf.set_font(family='helvetica', style='', size=24)
+    pdf.set_text_color(255, 255, 255)
+    pdf.y = 130
     pdf.cell(text='CS50 Shirtificate', center=True, new_y='TOP')
 
     pdf.output('shirtificate.pdf')
