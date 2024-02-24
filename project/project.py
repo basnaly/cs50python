b105d1ff4cb2ad35cc421f56007a1fbbe9f55433
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-24 11:57:25 +0000

    Sat, Feb 24, 2024, 1:57 PM +02:00

--- project/project.py
+++ project/project.py
@@ -1,12 +1,14 @@
 
 
-FARM_LIST = {
-    'carrot'
-}
+FARM_LIST = [
+    {'select': 1, 'type': 'vegatable', 'name': 'carrot', 'package': 'kg', 'price': '0.70'},
+    {'select': 2, 'type': 'greenery', 'name': 'parsel', 'package': 'bush', 'price': '0.20'},
+    {'select': 2,'type': 'fruit', 'name': 'apple', 'package': 'kg', 'price': '1.30'},
+]
 
 
 def main():
-    print('Welcome to our online farm store!')
+    print('Welcome to our online organic farm store!')
     print('You can order our fresh greenery, vegatables and fruit!')
 
 
