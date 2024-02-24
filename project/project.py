e239ce93abbef55b212a36f14200c13a7ea9c47e
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-24 12:18:15 +0000

    Sat, Feb 24, 2024, 2:18 PM +02:00

--- project/project.py
+++ project/project.py
@@ -11,7 +11,7 @@ def main():
     print('Welcome to our online organic farm store!')
     print('You can order our fresh greenery, vegatables and fruit from the list below:')
 
-    print('Pick an option:\n')
+    print('Pick you option with comma ',':\n')
     for index, item in enumerate(FARM_LIST):
         print(f'{index+1}) {item["name"]} {item["price"]}\n')
 
