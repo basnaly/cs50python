e7ea6117470484d6299b4007a38c699e5197713d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-24 11:50:34 +0000

    Sat, Feb 24, 2024, 1:50 PM +02:00

--- project/project.py
+++ project/project.py
@@ -1,9 +1,14 @@
 
 
+FARM_LIST = {
+    'carrot'
+}
+
+
 def main():
     print('Welcome to our online farm store!')
     print('You can order our fresh greenery, vegatables and fruit!')
-    
+
 
 
 if __name__ == '__main__':
