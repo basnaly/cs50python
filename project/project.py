1255d7cd86a17364c21cbe8798e1b2d11da2f296
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-24 12:02:46 +0000

    Sat, Feb 24, 2024, 2:02 PM +02:00

--- project/project.py
+++ project/project.py
@@ -1,9 +1,9 @@
 
 
 FARM_LIST = [
-    {'select': 1, 'type': 'vegatable', 'name': 'carrot', 'package': 'kg', 'price': '0.70'},
-    {'select': 2, 'type': 'greenery', 'name': 'parsel', 'package': 'bush', 'price': '0.20'},
-    {'select': 2,'type': 'fruit', 'name': 'apple', 'package': 'kg', 'price': '1.30'},
+    {'type': 'vegatable', 'name': 'carrot', 'package': 'kg', 'price': '0.70'},
+    {'type': 'greenery', 'name': 'parsel', 'package': 'bush', 'price': '0.20'},
+    {'type': 'fruit', 'name': 'apple', 'package': 'kg', 'price': '1.30'},
 ]
 
 
