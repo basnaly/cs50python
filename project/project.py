b9499b3740a8037096f132da0000ddc4b642db37
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-25 10:44:06 +0000

    Sun, Feb 25, 2024, 12:44 PM +02:00

--- project/project.py
+++ project/project.py
@@ -2,7 +2,8 @@
 
 FARM_LIST = [
     {'type': 'vegatable', 'name': 'carrot', 'icon': '🥕', 'price': '0.70/kg'},
-    {'type': 'fruit', 'name': 'banana', 'icon': '', 'price': '1.20/piece'},
+    {'type': 'fruit', 'name': 'banana', 'icon': '🍌', 'price': '1.20/kg'},
+    {'type': 'vegatable', 'name': 'cucumber', 'icon': '🥒', 'price': '0.60/kg'},
     {'type': 'fruit', 'name': 'apple', 'icon': '🍎', 'price': '1.30/kg'},
     {'type': 'vegatable', 'name': 'tomato', 'icon': '🍅', 'price': '0.90/kg'},
 ]
@@ -12,7 +13,7 @@ def main():
     print('Welcome to our online organic farm store!')
     print('You can order our fresh greenery, vegatables and fruit from the list below:')
 
-    print('Pick you option with comma ',':\n')
+    print('Pick you option: ')
     for index, item in enumerate(FARM_LIST):
         print(f'{index+1}) {item["name"]} {item["price"]}\n')
 
