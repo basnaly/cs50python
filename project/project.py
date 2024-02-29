6865c314d9366e55ab3696a9d8dce9bba76680ce
basnaly <89012819+basnaly@users.noreply.github.com>
2024-02-29 15:56:07 +0000

    Thu, Feb 29, 2024, 5:56 PM +02:00

--- project/project.py
+++ project/project.py
@@ -1,13 +1,25 @@
 import argparse
 
+ from create import create
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Procee one of three")
-    parser.add_argument('-m', '--mode', help="number of times to meow", type=str)
+    parser = argparse.ArgumentParser(description='Process one of three arguments')
+    parser.add_argument('-m', '--mode', help='Select one of three modes: create, edit or finish', type=str)
     args = parser.parse_args()
 
+    if args.mode == 'create':
+        create()
+    elif args.mode == 'edit':
+        print('Edit')
+    elif args.mode == 'finish':
+        print('Finish')
+    else:
+        print('Not supported option. Select one of three modes: create, edit or finish.')
 
 
 
-if __name__ == '__main__':
-    main()
+
+
+# if __name__ == '__main__':
+main()
