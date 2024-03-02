7dbd048215f70e344d0830cae957aee73f6d795f
basnaly <89012819+basnaly@users.noreply.github.com>
2024-03-02 08:09:03 +0000

    Sat, Mar 2, 2024, 10:09 AM +02:00

--- project/edit.py
+++ project/edit.py
@@ -3,9 +3,13 @@ import csv
 
 def edit():
     try:
+        table = []
         csv_file = 'basket.csv'
         with open(csv_file, mode='r') as file:
             reader = csv.DictReader(file)
+            for row in reader:
+                table.append(row)
+                
 
 
     except
