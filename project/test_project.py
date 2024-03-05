98010fb8573050be89a0eb5ceb24a20b1498a037
basnaly <89012819+basnaly@users.noreply.github.com>
2024-03-05 11:06:05 +0000

    Tue, Mar 5, 2024, 1:06 PM +02:00

--- project/test_project.py
+++ project/test_project.py
@@ -5,7 +5,7 @@ def test_product_calculate_sum():
     product = Product('Carrot', '🥕', 2.6)
     product.quantity = 2
     product.calculate_sum()
-
+    
     assert product.sum == 5.2
 
 
@@ -13,9 +13,9 @@ def test_get_product_obj():
     product = Product('Apple', '🍎', 4.3)
     product.quantity = 1.5
     product.sum = 6.45
-    product.get_product_obj()
+    product_obj = product.get_product_obj()
 
-    assert product.product_obj == {
+    assert product_obj == {
         'Name': 'Apple',
         'Icon': '🍎',
         'Price/Kg': 4.3,
