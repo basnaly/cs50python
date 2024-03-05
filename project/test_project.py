b60b7c5a0846913509a1eb88b706c2d8cbf88d4d
basnaly <89012819+basnaly@users.noreply.github.com>
2024-03-05 09:58:32 +0000

    Tue, Mar 5, 2024, 11:58 AM +02:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,6 +2,8 @@ from project import main
 
 
 def test_create_product():
-    product = Product()
-    
+    product = Product('Carrot', '🥕', '2.60/kg')
+    product.quantity = 2
+    product.sum = 5.2
+
     assert main([h] [-m MODE]) == True
